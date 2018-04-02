b0d2ad91a2da81be27537f3fc9ef9143a7aa23dc
Ed Heltzel <ed@portside.io>
2018-04-02 11:59:05 -0400

    random conflict

---
+++ fish/functions/siz.fish
@@ -0,0 +1,5 @@
+# Show directory size
+function siz
+  # du -khsc $argv | sort -rn
+  du -khsc $argv
+  end
--- fish/functions/sizes.fish
+++
@@ -1,5 +0,0 @@
-# # Show directory size
-# function size
-#   # du -khsc $argv | sort -rn
-#   du -khsc $argv
-#   end
