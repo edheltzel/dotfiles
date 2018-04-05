4fdbbb5d0ae8c5d7e7477ac96d31bb698d418ee3
Ed Heltzel <ed@portside.io>
2018-04-05 18:41:45 -0400

    commented out language stuff

--- fish/_exports.fish
+++ fish/_exports.fish
@@ -1,5 +1,5 @@
 set --export --global EDITOR code-insiders $EDITOR #set Visual Studio Code Insiders to default editor
 set --export --global VOL rdm $VOL 
 
-set --export --global LANG en_US $LANG
-set --export --global LC_ALL en_US.UTF-8 $LC_ALL
+#set --export --global LANG en_US $LANG
+#set --export --global LC_ALL en_US.UTF-8 $LC_ALL
