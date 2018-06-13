5d4c9adbb5d5be0a260a53f68f974d24a9208972
Ed Heltzel <ed@portside.io>
2018-06-13 16:25:51 -0400

    headed back to sublime for default editor

--- fish/_exports.fish
+++ fish/_exports.fish
@@ -1,5 +1,6 @@
-set --export --global EDITOR code-insiders $EDITOR #set Visual Studio Code Insiders to default editor
-set --export --global VOL rdm $VOL 
+#set --export --global EDITOR code-insiders $EDITOR #set Visual Studio Code Insiders to default editor
+set --export --global EDITOR subl $EDITOR #set Sublime Text to default editor
+set --export --global VOL rdm $VOL
 
 #set --export --global LANG en_US $LANG
 #set --export --global LC_ALL en_US.UTF-8 $LC_ALL
