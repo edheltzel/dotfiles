b3362b2e64aee7c308ecfdd3a7fa35f9757f2f31
Ed Heltzel <ed@rainyday.media>
2019-01-30 12:27:53 -0500

    neovim parts

--- vim/setup.sh
+++ vim/setup.sh
@@ -6,13 +6,49 @@ cd "$DIR"
 . ../scripts/functions.sh
 
 SOURCE="$(realpath .)"
-DESTINATION="$(realpath ~)"
+DESTINATION="$(realpath ~/.config/nvim)"
 
-info "Configuring misc dotfiles..."
+info "Setting up fish shell..."
 
-find . -name ".*" | while read fn; do
-    fn=$(basename $fn)
+substep_info "Creating fish config folders..."
+mkdir -p "$DESTINATION/functions"
+mkdir -p "$DESTINATION/completions"
+
+find * -name "*fish*" | while read fn; do
     symlink "$SOURCE/$fn" "$DESTINATION/$fn"
 done
+clear_broken_symlinks "$DESTINATION"
 
-success "Finished configuring misc dotfiles."
+
+set_fish_shell() {
+    if grep --quiet fish <<< "$SHELL"; then
+        success "Fish shell is already set up."
+    else
+        substep_info "Adding fish executable to /etc/shells"
+        if grep --fixed-strings --line-regexp --quiet "/usr/local/bin/fish" /etc/shells; then
+            substep_success "Fish executable already exists in /etc/shells."
+        else
+            if sudo bash -c "echo /usr/local/bin/fish >> /etc/shells"; then
+                substep_success "Fish executable added to /etc/shells."
+            else
+                substep_error "Failed adding Fish executable to /etc/shells."
+                return 1
+            fi
+        fi
+        substep_info "Changing shell to fish"
+        if sudo chsh -s /usr/local/bin/fish; then
+            substep_success "Changed shell to fish"
+        else
+            substep_error "Failed changing shell to fish"
+            return 2
+        fi
+        substep_info "Running fish initial setup"
+        fish -c "setup"
+    fi
+}
+
+if set_fish_shell; then
+    success "Successfully set up fish shell. Make sure you run Fisher manually"
+else
+    error "Failed setting up fish shell."
+fi
