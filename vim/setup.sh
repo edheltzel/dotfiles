fce7f975f6a25f06fde0c0b451d157cc93cf8477
Ed Heltzel <ed@rainyday.media>
2019-09-10 12:30:27 -0400

    moved back to regular vim

--- vim/setup.sh
+++ vim/setup.sh
@@ -6,16 +6,13 @@ cd "$DIR"
 . ../scripts/functions.sh
 
 SOURCE="$(realpath .)"
-DESTINATION="$(realpath ~/.config/nvim)"
+DESTINATION="$(realpath ~)"
 
-info "Setting up neovim..."
+info "Setting up Vim..."
 
-substep_info "Creating nvim plug folder..."
-mkdir -p "$DESTINATION/autoload"
-
-find * -name "*vim*" | while read fn; do
+find . -name ".vim*" | while read fn; do
+    fn=$(basename $fn)
     symlink "$SOURCE/$fn" "$DESTINATION/$fn"
 done
-clear_broken_symlinks "$DESTINATION"
 
-success "Finished configuring neovim. Make sure you run :PlugInstall inside of nvim"
+success "Finished setting up Vim."
