89b5fadb0ec0266a0af227a403b9307f49a97c75
Ed Heltzel <ed@rainyday.media>
2019-09-10 21:58:13 -0400

    fix exports

--- fish/_exports.fish
+++ fish/_exports.fish
@@ -1,12 +1,9 @@
-set --export --global EDITOR code-insiders $EDITOR #set Visual Studio Code Insiders as default editor use code, code-insiders, subl or vim
-set --export --global VOL rdm $VOL
+set -x --global EDITOR code-insiders $EDITOR #set Visual Studio Code Insiders as default editor use code, code-insiders, subl or vim
+set -x --global VOL rdm $VOL
 
 # GoLang
-set -x GOPATH ~/Development/Go
+set -x GOPATH ~/Projects/Go
 set -x GOROOT /usr/local/opt/go/libexec
 
 set -x PATH $PATH $GOROOT/bin $GOPATH/bin
 
-
-#set --export --global LANG en_US $LANG
-#set --export --global LC_ALL en_US.UTF-8 $LC_ALL
