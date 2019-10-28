7cbfa31885da721bc10c70caa06653d8540b89d4
Ed Heltzel <ed@rainyday.media>
2019-10-28 15:05:30 -0400

    adds some changes to fish abbr.

--- fish/functions/setup.fish
+++ fish/functions/setup.fish
@@ -1,26 +1,28 @@
 ## Defines abbreviations
 function setup --description 'defines abbreviations'
-  abbr g git
+  abbr gg go get GITHUB_URL
   abbr gs git status
+  abbr gd git diff
   abbr gc git cm
   abbr ga git aa
   abbr gp git push
-  abbr gd git diff
   abbr gco git co
+  abbr gpl git pull
+  abbr gma git aa; and git cm
   abbr gb git b -a
   abbr binfo brew info
   abbr bi brew install
   abbr brews brew leaves
   abbr cl clear
   abbr del trash
-  abbr gems gem query --local
+  abbr sdel rm -rf
   abbr rubys rbenv install -l
+  abbr gems gem query --local
   abbr gemup gem update --system
   abbr nig npm install -g
   abbr npms npm list -g --depth=0
   abbr op open .
-  abbr sdel rm -rf
-  abbr sp serverpilot
+  abbr opa open -a APP_NAME
   abbr rc runcloud
   abbr symlink ln -s
   abbr tkill tmux kill-session -t
