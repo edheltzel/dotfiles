c6455f528a229608c45b661f208fc15fda88eca3
Ed Heltzel <ed@rainyday.media>
2019-12-06 15:29:31 -0500

    fd conflict

--- fish/functions/setup.fish
+++ fish/functions/setup.fish
@@ -27,5 +27,4 @@ function setup --description 'defines abbreviations'
   abbr symlink ln -s
   abbr tkill tmux kill-session -t
   abbr suckit wget -mkEpnp url_here
-  abbr fd git@github.com:ginfuru/Flightdeck.git
 end
