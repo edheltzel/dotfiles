5e61ce84823049b67409acbacb97de4e3876017f
Ed Heltzel <ed@rainyday.media>
2020-03-25 10:32:44 -0400

    removes the reverse flag on the custom cd function

--- fish/functions/cd.fish
+++ fish/functions/cd.fish
@@ -3,6 +3,6 @@ function cd --description 'Utility for expanding abbreviations'
         # prevents recurse infinitely by using built-in cd
         builtin cd "$argv"; and exa -Flahr --group-directories-first
     else
-        builtin cd ~; and exa -Flahr --group-directories-first
+        builtin cd ~; and exa -Flah --group-directories-first
     end
 end
