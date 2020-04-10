2016ba2022b95b075d5ca6dc219dbd9fc0fca6bd
Ed Heltzel <ed@rainyday.media>
2020-04-10 14:39:50 -0400

    better comment description

--- fish/customs/devops_workflow.fish
+++ fish/customs/devops_workflow.fish
@@ -14,17 +14,16 @@ function pubkey
     cat ~/.ssh/id_rsa.pub | pbcopy; and echo '=> Public key copied to clipboard.'
 end
 
-# Network
+# Often used shortcuts/aliases for Network
 	alias ip="dig +short myip.opendns.com @resolver1.opendns.com" # dumps [YOUR PUBLIC IP] [URL IP]
 	alias localip="ipconfig getifaddr en0" #internal network IP
 	alias ips="ifconfig -a | grep -o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\|[a-fA-F0-9:]\+\)' | sed -e 's/inet6* //'"
 
-# View HTTP traffic
+# Often used shortcuts/aliases for View HTTP traffic
 	alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
 	alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 
-# Docker
+# Often used shortcuts/aliases for Docker
 function dcons; docker container ls --all; end
 function dc; docker-compose; end
 function dcrun; docker-compose run --rm; end
-
