9b4cd1138ce74d2bc512afef9c30872dd043b07f
Ed Heltzel <ed@rainyday.media>
2020-04-26 10:27:17 -0400

    remove brew vim and tmuxinator

--- fish/config.fish
+++ fish/config.fish
@@ -11,6 +11,4 @@ eval (starship init fish)
 #fish_vi_key_bindings
 
 # Using Homebrew Ruby
-set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
-#set -U fish_user_paths (ruby -e 'print Gem.user_dir')/bin $fish_user_paths
-#set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
+#set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
