476cc37766912f39112d201ff312ee1f6c66c3a2
Ed Heltzel <ed@rainyday.media>
2020-05-26 23:46:31 -0400

    removes `yank.sh` in favor of tmux plugin adds auto tmux loading adds amethyst window management

--- fish/conf.d/tmux.fish
+++ fish/conf.d/tmux.fish
@@ -1,6 +1,6 @@
 # as seen https://medium.com/@HazuliFidastian/run-tmux-automatically-on-fish-shell-2b62622661c7
 if not set -q TMUX
-    set -g TMUX tmux new-session -d -s main
+    set -g TMUX tmux new-session -d -s base
     eval $TMUX
-    tmux attach-session -d -t main
+    tmux attach-session -d -t base
 end
