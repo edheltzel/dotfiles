6e63954371f04cd8fc7485bccd7d3f81ba71108e
Ed Heltzel <ed@rainyday.media>
2020-05-29 13:41:54 -0400

    hot corner updates for macos

--- macos/setup.sh
+++ macos/setup.sh
@@ -464,11 +464,11 @@ sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/Simulator (
 # 12: Notification Center
 
 # Top left screen corner → Mission Control
-defaults write com.apple.dock wvous-tl-corner -int 2
+defaults write com.apple.dock wvous-tl-corner -int 3
 defaults write com.apple.dock wvous-tl-modifier -int 0
 
 # Top right screen corner → Nofication Center
-defaults write com.apple.dock wvous-tr-corner -int 12
+defaults write com.apple.dock wvous-tr-corner -int 4
 defaults write com.apple.dock wvous-tr-modifier -int 0
 
 # Bottom right screen corner → Desktop
@@ -476,7 +476,7 @@ defaults write com.apple.dock wvous-br-corner -int 4
 defaults write com.apple.dock wvous-br-modifier -int 0
 
 # Bottom left screen corner → Desktop
-defaults write com.apple.dock wvous-bl-corner -int 4
+defaults write com.apple.dock wvous-bl-corner -int 2
 defaults write com.apple.dock wvous-bl-modifier -int 0
 
 ###############################################################################
