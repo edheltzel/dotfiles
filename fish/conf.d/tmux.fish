98ef7b7470bbe94e8d7c4740797e013c94f66dd0
Ed Heltzel <ed@rainyday.media>
2020-05-28 20:11:13 -0400

    brew file updates

--- fish/conf.d/tmux.fish
+++ fish/conf.d/tmux.fish
@@ -1,6 +1,8 @@
 # as seen https://medium.com/@HazuliFidastian/run-tmux-automatically-on-fish-shell-2b62622661c7
-if not set -q TMUX
-    set -g TMUX tmux new-session -d -s base
-    eval $TMUX
-    tmux attach-session -d -t base
-end
+# currently using iTerm with system preferrences to auto attach a session
+# if using Alacritty - uncomment ↓
+# if not set -q TMUX
+#    set -g TMUX tmux new-session -d -s base
+#    eval $TMUX
+#    tmux attach-session -d -t base
+# end
