8d8e0188e89b9c51985be302e5476d77194d9d4d
Ed Heltzel <ed@rainyday.media>
2020-05-29 13:42:32 -0400

    updates go paths

--- fish/customs/_exports.fish
+++ fish/customs/_exports.fish
@@ -2,8 +2,10 @@
 set -x --global EDITOR code-insiders $EDITOR #set Visual Studio Code Insiders as default editor use code, code-insiders, subl or vim
 set -x --global VOL rdm $VOL
 
-# GoLang
-set -x -U GOPATH $HOME/.config/Go
+# GOLANG configurations
+set -x GOPATH ~/Projects/Go
+set -x GOROOT /usr/local/opt/go/libexec
+set -x PATH $PATH $GOROOT/bin $GOPATH/bin
 
 # fzf and fd helpers for NeoVim
 set -x FZF_DEFAULT_COMMAND "fd --type f"
