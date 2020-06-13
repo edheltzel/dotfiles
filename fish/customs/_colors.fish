5ac74fe79503ee4220b226276f9aa4623071e23d
Ed Heltzel <ed@rainyday.media>
2020-06-12 23:54:50 -0400

    adds full output of fishprompt

--- fish/customs/_colors.fish
+++ fish/customs/_colors.fish
@@ -10,6 +10,30 @@ set -x -g EXA_COLORS "gm=33;1"
 set -x -g TERM "xterm-256color"
 
 # Prompt mods
-set fish_color_error red
-set fish_color_command white --bold
+set fish_color_autosuggestion 555 brblack
+set fish_color_cancel -r
+set fish_color_command --bold
+set fish_color_comment red
+set fish_color_cwd green
+set fish_color_cwd_root red
+set fish_color_end brmagenta
+set fish_color_error brred
+set fish_color_escape bryellow --bold
+set fish_color_history_current --bold
+set fish_color_host normal
+set fish_color_host_remote yellow
+set fish_color_match --background=brblue
+set fish_color_normal normal
+set fish_color_operator bryellow
 set fish_color_param cyan
+set fish_color_quote yellow
+set fish_color_redirection brblue
+set fish_color_search_match bryellow --background=brblack
+set fish_color_selection white --bold --background=brblack
+set fish_color_status red
+set fish_color_user brgreen
+set fish_color_valid_path --underline
+set fish_pager_color_completion
+set fish_pager_color_description B3A06D yellow
+set fish_pager_color_prefix white --bold --underline
+set fish_pager_color_progress brwhite --background=cyan
