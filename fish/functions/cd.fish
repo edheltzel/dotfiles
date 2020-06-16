7d8a83c2869badd9130db2e7e5b1ae1955a57797
Ed Heltzel <ed@rainyday.media>
2020-06-16 14:35:59 -0400

    adds sort option to cd function

--- fish/functions/cd.fish
+++ fish/functions/cd.fish
@@ -1,8 +1,8 @@
 function cd --description 'Utility for expanding abbreviations'
     if count $argv > /dev/null
         # prevents recurse infinitely by using built-in cd
-        builtin cd "$argv"; and exa -Flahr --group-directories-first
+        builtin cd "$argv"; and exa -Flah --sort name --group-directories-first
     else
-        builtin cd ~; and exa -Flah --group-directories-first
+        builtin cd ~; and exa -Flah --sort name --group-directories-first
     end
 end
