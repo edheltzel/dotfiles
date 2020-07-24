e2b582ce3614fda82261f060a86de06073fb6566
Ed Heltzel <ed@rainyday.media>
2020-07-24 18:27:21 -0400

    start of macos individual files

---
+++ macos/_02-desktop_screensaver.sh
@@ -0,0 +1,25 @@
+################################################################################
+# Desktop & Screen Saver                                                       #
+################################################################################
+
+# Set a custom wallpaper image. `DefaultDesktop.jpg` is already a symlink, and
+# all wallpapers are in `/Library/Desktop Pictures/`. The default is `Wave.jpg`.
+#rm -rf ~/Library/Application Support/Dock/desktoppicture.db
+#sudo rm -rf /System/Library/CoreServices/DefaultDesktop.jpg
+#sudo ln -s /path/to/your/image /System/Library/CoreServices/DefaultDesktop.jpg
+
+
+# Set Screen Savers
+defaults -currentHost write com.apple.screensaver moduleDict "
+  <dict>
+    <key>moduleName</key><string>Shell</string>
+    <key>path</key><string>/System/Library/Screen Savers/Drift.saver</string>
+    <key>type</key><integer>1</integer>
+  </dict>
+"
+
+# Start after 30 Minutes
+defaults -currentHost write com.apple.screensaver idleTime -int 1800
+
+# Show with clock (default)
+defaults -currentHost write com.apple.screensaver showClock -bool false
