84bd01f6d445c3856f9b9e319202ae07d837c182
Ed Heltzel <ed@rainyday.media>
2021-01-28 14:45:39 -0500

    abbrivation

--- fish/functions/setup.fish
+++ fish/functions/setup.fish
@@ -7,7 +7,7 @@ function setup --description 'defines abbreviations'
   abbr casks brew list --cask
   abbr cl clear
   abbr clone git clone
-  abbr co code .
+  abbr co code-insiders .
   abbr del trash
   abbr dls ~/Downloads/
   abbr ga git aa
