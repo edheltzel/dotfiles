562f0de27d66eaafe16dab71419d0beedd6d72b6
Ed Heltzel <ed@rainyday.media>
2021-01-30 21:52:54 -0500

    brewfile update - adds bottom `btm` with custom function - drops ytop in favor of bottom -- ytop is no longer being developed. - adds tokei to displays statistics about your code - adds fd to replace find - adds ripgrep to replace grep - changes duti for svg to sketch

---
+++ fish/functions/btm.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function btm --description 'alias btm=btm --battery --fahrenheit'
+  command btm --battery --fahrenheit;
+end
--- fish/functions/ytop.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function ytop --description 'alias ytop=ytop -f'
-  command ytop --fahrenheit;
-end
