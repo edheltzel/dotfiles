b088cd9c36c031ce718676d7b537bf377ea01368
Ed Heltzel <ed@rainyday.media>
2021-03-08 11:29:56 -0500

    rbenv issues

--- fish/config.fish
+++ fish/config.fish
@@ -16,8 +16,8 @@ set -g fish_user_paths "/opt/homebrew/bin" $fish_user_paths # homebrew ARM
 set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
 
 # rbenv
-set -Ux fish_user_paths $HOME/.rbenv/bin $fish_user_paths
-eval (rbenv init - | source )
+# set -Ux fish_user_paths $HOME/.rbenv/bin $fish_user_paths
+# eval (rbenv init - | source )
 
 # openssl to path
 set -g fish_user_paths "/opt/homebrew/opt/openssl@1.1/bin" $fish_user_paths
