a41b3ed9292e119aa3177089c645429951c236d4
Ed Heltzel <ed@rainyday.media>
2021-07-18 08:51:42 -0500

    fisher update and upp function change

--- fish/functions/upp.fish
+++ fish/functions/upp.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 0
 function upp --description 'work around for nvm to work with topgrade'
-  node --version; and topgrade
+  echo 'node version in use:'; and node --version ; and topgrade
 end
