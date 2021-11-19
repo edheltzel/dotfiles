e9dd327c3d1ab8dc2f904003c74238eda9519c7c
Ed Heltzel <ed@rainyday.media>
2021-11-19 09:28:04 -0500

    updates package adds new fish function - removes homebrew speedtest package - adds new speedtest function
    speedtest function uses the macOS native `networkQuality` cli
    this checks the network download and upload speeds.

---
+++ fish/functions/speedtest.fish
@@ -0,0 +1,11 @@
+# Defined in - @ line 0
+# checks for option/flags and passes it to the alias
+# if no options run with verbose option.
+
+function speedtest --description 'alias speedtest=networkQuality'
+  if count $argv > /dev/null #Checks for option
+  command networkQuality $argv
+  else
+  command networkQuality -v #No option - run verbose
+  end
+end
