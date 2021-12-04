6c9ec2a8d0b7829b067f32d9420e13c50a96d993
Ed Heltzel <ed@rainyday.media>
2021-12-04 07:16:12 -0500

    adds vscode folder for vscode workflow

---
+++ vscode/setup.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env sh
+
+DIR=$(dirname "$0")
+cd "$DIR"
+
+. ../scripts/functions.sh
+
+SOURCE="$(realpath -m .)"
+DESTINATION="$(realpath -m ~/.config/vscode)"
+
+info "Setting up Visual Studio Code..."
+
+substep_info "Creating Visual Studio Code folders..."
+mkdir -p "$DESTINATION"
+
+find * -not -name "$(basename ${0})" -type f | while read fn; do
+    symlink "$SOURCE/$fn" "$DESTINATION/$fn"
+done
+
+success "Finished setting up Visual Studio Code"
