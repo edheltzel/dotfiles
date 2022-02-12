3323220348caf67a9f7b2e871b2d219926ab1907
Ed Heltzel <ed@rainyday.media>
2022-02-12 10:31:08 -0500

    adds brew cleanup

--- fish/functions/upp.fish
+++ fish/functions/upp.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 0
 function upp --description 'work around for nvm to work with topgrade'
-  echo 'node version in use:'; and node --version ; and topgrade
+  echo 'node version in use:'; and node --version; and topgrade; and brew cleanup --prune=all
 end
