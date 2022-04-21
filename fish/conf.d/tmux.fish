06192153b508470e88fb77678302dae5d88f284f
Ed Heltzel <ed@rainyday.media>
2022-04-21 15:57:52 -0400

    removed fisher plugin `conf.d` files caused fisher to break -- TODO investigate symlinking

--- fish/conf.d/tmux.fish
+++ fish/conf.d/tmux.fish
@@ -1,8 +1,7 @@
 # as seen https://medium.com/@HazuliFidastian/run-tmux-automatically-on-fish-shell-2b62622661c7
-# currently using iTerm with system preferrences to auto attach a session
-# if using Alacritty - uncomment ↓
-# if not set -q TMUX
-#    set -g TMUX tmux new-session -d -s base
-#    eval $TMUX
-#    tmux attach-session -d -t base
-# end
+# Uncomment the below if-statement if using Alacritty
+if not set -q TMUX
+   set -g TMUX tmux new-session -d -s base
+   eval $TMUX
+   tmux attach-session -d -t base
+end
