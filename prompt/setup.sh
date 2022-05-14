1d3397ad005117cda05bac96b94224e33b0f65be
Ed Heltzel <ed@rainyday.media>
2022-05-14 11:01:49 -0400

    clean up startship and fixed spacing is setup.sh

--- prompt/setup.sh
+++ prompt/setup.sh
@@ -13,6 +13,7 @@ info "Setting up Starship prompt & Topgrade..."
 find * -name "*.toml*" | while read fn; do
     symlink "$SOURCE/$fn" "$CONFIG_PATH/$fn"
 done
+
 clear_broken_symlinks "$CONFIG_PATH"
 
 success "Finished configuring Starship prompt & Topgrade."
