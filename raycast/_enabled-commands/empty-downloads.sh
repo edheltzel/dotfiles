9696b001c671b077309c95afbd93b18dcc65c54b
Ed Heltzel <ed@rainyday.media>
2022-05-28 14:55:04 -0400

    adds empty download script to raycast

---
+++ raycast/_enabled-commands/empty-downloads.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Empty Downloads
+# @raycast.mode inline
+# @raycast.packageName System
+#
+# Optional parameters:
+# @raycast.icon 🗑️
+
+# Documentation:
+# @raycast.description Clean out ~/Downloads
+# @raycast.author Ed Heltzel
+# @raycast.authorURL https://github.com/edheltzel
+
+ rm -rf ~/Downloads/*
