c456c0a8ed66e2809848ee451a239a90176bb2f2
Ed Heltzel <ed@rainyday.media>
2022-08-17 15:25:26 -0400

    utilities update - tig config to include highlighting during diff - nirc added - fish package update

--- fish/config.fish
+++ fish/config.fish
@@ -54,3 +54,7 @@ set -x --global EDITOR code
 
 #set primary volume for workstation
 set -x --global VOL xxx
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/__tabtab.fish ]; and . ~/.config/tabtab/__tabtab.fish; or true
