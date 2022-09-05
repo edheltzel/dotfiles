cb85a91202fb9e1732e28306cef822bc5741d8ab
Ed Heltzel <ed@rainyday.media>
2022-09-05 08:22:58 -0400

    vscode terminal integration with fish - and iterm update

--- fish/config.fish
+++ fish/config.fish
@@ -52,6 +52,9 @@ starship init fish | source
 # set Visual Studio Code as default editor use code, code-insiders, subl or vim
 set -x --global EDITOR code
 
+string match -q "$TERM_PROGRAM" "vscode"
+and . (code --locate-shell-integration-path fish)
+
 #set primary volume for workstation
 set -x --global VOL xxx
 
