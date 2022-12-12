198d4da383d08f00f323e818decf768b323f7124
Ed Heltzel <ed@rainyday.media>
2022-12-12 10:47:25 -0500

    updates to topgrade toml and to `upp` fish function - adds pnpm corepack at the end of `upp` - ignores git_repos in topgrade

--- fish/functions/upp.fish
+++ fish/functions/upp.fish
@@ -4,7 +4,8 @@ function upp --description 'Updates everything with Topgrade but also prints the
   set nodeIcon ' '; set nodeVerb 'Node Version in use: '; set_color green --bold;
   printf "$nodeIcon"; set_color normal;
   printf "$nodeVerb"; set_color green --bold; and node --version; set_color white;
-  topgrade
+  topgrade --cleanup; set_color white;
   printf '\n―― '(date "+%H:%M:%S")'- Brew - Cleanup ――――――――――――――――――――――――――――――――――――――――――――――――――――\n'; set_color normal; and brew cleanup --prune=all; set_color white;
-  printf '\n―― '(date "+%H:%M:%S")'- Z - Cleanup ――――――――――――――――――――――――――――――――――――――――――――――――――――――\n'; set_color normal; and z --clean;
+  printf '\n―― '(date "+%H:%M:%S")'- Z - Cleanup ――――――――――――――――――――――――――――――――――――――――――――――――――――――\n'; set_color normal; and z --clean; set_color white;
+  printf '\n―― '(date "+%H:%M:%S")'- pnpm - Corepack  ――――――――――――――――――――――――――――――――――――――――――――――――――――――\n'; set_color normal; and corepack prepare pnpm@latest --activate;
 end
