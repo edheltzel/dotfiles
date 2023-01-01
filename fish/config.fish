c83999d90af8e8253cc044767dcb90cca23341c8
Ed Heltzel <ed@rainyday.media>
2023-01-01 10:59:38 -0500

    adding vi mode to fish

--- fish/config.fish
+++ fish/config.fish
@@ -61,3 +61,6 @@ set -x --global VOL xxx
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/__tabtab.fish ]; and . ~/.config/tabtab/__tabtab.fish; or true
+
+# enable Vi mode
+fish_vi_key_bindings
