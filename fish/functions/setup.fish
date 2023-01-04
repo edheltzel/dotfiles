cc79a4ab108672cd0b58c22247b54e0e99d46e36
Ed Heltzel <ed@rainyday.media>
2023-01-04 09:26:44 -0500

    abbr update - removed unused

--- fish/functions/setup.fish
+++ fish/functions/setup.fish
@@ -4,60 +4,28 @@ function setup --description 'defines abbreviations'
   abbr binfo brew info
   abbr brews brew leaves
   abbr casks brew list --cask
-  abbr cl clear
-  abbr clone git clone
-  abbr cma commit-all
   abbr co code .
   abbr con code -n .
   abbr config ~/.config/
   abbr coo code -r .
-  abbr cor code -r .
   abbr del trash
   abbr dls ~/Downloads/
-  abbr ga stage
-  abbr gb git b -a
-  abbr gcm commit
-  abbr gco git co
-  abbr gcs show
-  abbr gd git diff
   abbr gems gem list
   abbr gg go get GITHUB_URL
   abbr ghw gh repo view --web
-  abbr gma git aa
-  abbr gmac commit-all
-  abbr gmca commit-all
   abbr goo cd ~/.go/
   abbr npms npm list -g --depth=0
-  abbr nad npm add -D
-  abbr nag npm add -g
-  abbr nrd npm rm -D
-  abbr nrg npm rm -g
-  abbr pad pnpm add -D
-  abbr pag pnpm add -g
-  abbr pi pnpm install
-  abbr pr pnpm run
-  abbr pss pnpm start
   abbr pns pnpm list -g
   abbr pnpms pnpm list -g
-  abbr prd pnpm rm -D
-  abbr prg pnpm rm --global
-  abbr pug pnpm add -g pnpm
-  abbr pugg pn upgrade -g
-  abbr pup pnpm up
-  abbr op open .
-  abbr opa open -a APP_NAME
   abbr sdel rm -rf
   abbr siz du -khsc
-  abbr sp speedtest -s
+  abbr sp speedtest
   abbr suckit wget -mkEpnp url_here
   abbr symlink ln -s
-  abbr mksym ln -s
   abbr amux tmux at -t base
   abbr tkill tmux kill-session -t
   abbr nmux tmux new -s "base"
   abbr zpurge z --clean
-  abbr cma commit-all
-  abbr mm merge master
   abbr lg lazygit
   abbr lzd lazydocker
 end
