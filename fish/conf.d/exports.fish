5a44af4a4d1f3dc5b57ec6b60e165e3c129290bb
Ed Heltzel <ed@rainyday.media>
2023-01-08 11:38:41 -0500

    default fish key bindings set in exports.fish

--- fish/conf.d/exports.fish
+++ fish/conf.d/exports.fish
@@ -1,3 +1,6 @@
+# set default key bindings
+set -g fish_key_bindings fish_default_key_bindings
+
 # Source GRC
 # https://github.com/oh-my-fish/plugin-grc/issues/20#issue-296031557
 source /opt/homebrew/etc/grc.fish
