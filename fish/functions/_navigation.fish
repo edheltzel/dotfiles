3421295272ea8fb7f91c5bc6c7fc7138ba9338c2
Ed Heltzel <ed@rainyday.media>
2023-01-26 18:50:07 +0100

    ls command comment

--- fish/functions/_navigation.fish
+++ fish/functions/_navigation.fish
@@ -8,7 +8,7 @@ function l; command exa -Flagh --sort name --git --icons --group-directories-fir
 function ll; command exa -Flagh --git --icons --group-directories-first --sort modified $argv; end
 function la; command exa -Fla --icons; end
 function tree; command exa --tree --icons $argv; end
-# function ls; command exa $argv; end
+# function ls; command exa $argv; end # using native ls for backwards compatibility in some cases
 function cll; command clear; and exa -Flah --icons --sort modified $argv; end
 
 # Often used shortcuts/aliases
