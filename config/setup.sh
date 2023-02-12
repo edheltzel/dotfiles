6a3f8bf160e321a0b5727fcb29a715cb9e9f47ab
Ed Heltzel <ed@rainyday.media>
2023-02-12 18:29:00 -0500

    moves everything to config/ updates package/setup.sh  to include fnm and node install

--- config/setup.sh
+++ config/setup.sh
@@ -9,21 +9,45 @@ SOURCE="$(realpath .)"
 DESTINATION="$(realpath ~)"
 CONFIG_PATH="$(realpath ~/.config)"
 
-info "Configuring ~/.config and home dotfiles"
+info "Setting symlinks dotfiles and config folders."
 
+substep_info "Symlink dotfiles to home directory..."
 find . -name ".*" | while read fn; do
     fn=$(basename $fn)
     symlink "$SOURCE/$fn" "$DESTINATION/$fn"
 done
+substep_success "Successfully created symlinks."
 
+substep_info "Symlinking ~/.config folders..."
 find . -name "bat" | while read fn; do
     fn=$(basename $fn)
     symlink "$SOURCE/$fn" "$CONFIG_PATH/$fn"
 done
+substep_success "Bat is ready."
 
 find . -name "raycast" | while read fn; do
     fn=$(basename $fn)
     symlink "$SOURCE/$fn" "$CONFIG_PATH/$fn"
 done
+substep_success "Raycast is ready."
 
-success "Finished setting up .config "
+find . -name "iterm" | while read fn; do
+    fn=$(basename $fn)
+    symlink "$SOURCE/$fn" "$CONFIG_PATH/$fn"
+done
+substep_success "iTerm is ready."
+
+find . -name "karabiner" | while read fn; do
+    fn=$(basename $fn)
+    symlink "$SOURCE/$fn" "$CONFIG_PATH/$fn"
+done
+substep_success "Karabiner is ready."
+
+find * -name "*.toml*" | while read fn; do
+    symlink "$SOURCE/$fn" "$CONFIG_PATH/$fn"
+done
+substep_success "Starship prompt & Topgrade are ready."
+
+clear_broken_symlinks "$CONFIG_PATH"
+
+success "Finished setting up ~/.config"
