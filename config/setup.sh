39e0a0fafca773ce45bd1e7b0135b2e8cbbd0b64
Ed Heltzel <ed@rainyday.media>
2023-02-12 18:30:16 -0500

    config setup.sh adds symlinks and install of astrovim

--- config/setup.sh
+++ config/setup.sh
@@ -8,6 +8,8 @@ cd "$DIR"
 SOURCE="$(realpath .)"
 DESTINATION="$(realpath ~)"
 CONFIG_PATH="$(realpath ~/.config)"
+NVIM_PATH="$(realpath ~/.config/nvim)"
+
 
 info "Setting symlinks dotfiles and config folders."
 
@@ -19,35 +21,70 @@ done
 substep_success "Successfully created symlinks."
 
 substep_info "Symlinking ~/.config folders..."
+
+#Starship prompt & Topgrade setup
+find * -name "*.toml*" | while read fn; do
+    symlink "$SOURCE/$fn" "$CONFIG_PATH/$fn"
+done
+substep_success "Starship prompt & Topgrade are ready."
+
+#bat setup
 find . -name "bat" | while read fn; do
     fn=$(basename $fn)
     symlink "$SOURCE/$fn" "$CONFIG_PATH/$fn"
 done
 substep_success "Bat is ready."
 
+#Raycast setup
 find . -name "raycast" | while read fn; do
     fn=$(basename $fn)
     symlink "$SOURCE/$fn" "$CONFIG_PATH/$fn"
 done
 substep_success "Raycast is ready."
 
+#iTerm setup
 find . -name "iterm" | while read fn; do
     fn=$(basename $fn)
     symlink "$SOURCE/$fn" "$CONFIG_PATH/$fn"
 done
 substep_success "iTerm is ready."
 
+#Karabiner setup
 find . -name "karabiner" | while read fn; do
     fn=$(basename $fn)
     symlink "$SOURCE/$fn" "$CONFIG_PATH/$fn"
 done
 substep_success "Karabiner is ready."
 
-find * -name "*.toml*" | while read fn; do
+#VSCode setup
+find . -name "vscode" | while read fn; do
     symlink "$SOURCE/$fn" "$CONFIG_PATH/$fn"
 done
-substep_success "Starship prompt & Topgrade are ready."
+substep_success "VSCode is ready."
+
+#Github GH setup
+find . -name "gh" | while read fn; do
+    symlink "$SOURCE/$fn" "$CONFIG_PATH/$fn"
+done
+substep_success "GH is ready. Make sure to copy hosts.yml from secrets folder to ~/.config/gh/hosts.yml"
+
+
+
+
+#NeoVim AstroVim setup
+substep_info "Git is cloning AstroVim..."
+mkdir -p $NVIM_PATH
+git --progress -C "$NVIM_PATH" remote set-url origin https://github.com/AstroNvim/AstroNvim.git
+
+substep_success "Successfully cloned AstroVim"
+
+substep_info "Initializing AstroNvim"
+nvim  --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
+substep_success "Nvim is ready."
+
 
 clear_broken_symlinks "$CONFIG_PATH"
 
-success "Finished setting up ~/.config"
+success "Home Directory and Config folders configured."
+
