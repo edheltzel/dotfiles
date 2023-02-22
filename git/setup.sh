54857339e169a23861c44fb75ec0d0d38605e925
Ed Heltzel <ed@rainyday.media>
2023-02-22 07:37:30 -0500

    adds git/ - moves gitconfig to git/ - moves gitignore_global to git/ - includes gitconfig-local to git/

---
+++ git/setup.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env sh
+
+DIR=$(dirname "$0")
+cd "$DIR"
+
+. ../scripts/functions.sh
+
+SOURCE="$(realpath .)"
+DESTINATION="$(realpath ~)"
+
+info "Setting symlinks for Git."
+
+find . -maxdepth 1 -name ".*" -not -wholename "*DS_Store*" | while read fn; do
+    fn=$(basename $fn)
+    symlink "$SOURCE/$fn" "$DESTINATION/$fn"
+done
+substep_success "Successfully created symlinks."
+
+clear_broken_symlinks
+
+success "Git configured."
