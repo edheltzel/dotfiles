79a28e2df07d972db4d06da243b7d4a4c8ae3333
Ed Heltzel <ed@rainyday.media>
2023-02-23 09:23:36 -0500

    node setup script.

--- node/setup.sh
+++ node/setup.sh
@@ -9,28 +9,47 @@ COMMENT=\#*
 
 sudo -v
 
-info "Installing FNM - Fast and simple NodeJS version manager..."
-curl -fsSL https://fnm.vercel.app/install | bash
+substep_info "👀 Checking for FNM - Fast and simple NodeJS version manager..."
+if test ! $(which fnm); then
+    info "Installing FNM..."
+    if /bin/bash -c "$(curl -fsSL https://fnm.vercel.app/install)"; then
+        success "✅ FNM is now installed."
+    else
+        error "❌ Failed to install FNM."
+    fi
+else
+    substep_success "👍 FNM - already installed."
+fi
 
-substep_info "Installing Node LTS with Corepack enabled"
-fnm install --lts && npm i -g corepack && corepack enable
 
-success "Finished installing Node with FNM."
+substep_info "Installing Node LTS set as default..."
+fnm install --lts && fnm alias lts-latest default && fnm use default
 
-find * -name "*.list" | while read fn; do
-    cmd="${fn%.*}"
-    set -- $cmd
-    info "Installing $1 packages..."
-    while read package; do
-        if [[ $package == $COMMENT ]];
-        then continue
-        fi
-        substep_info "Installing $package..."
-        if [[ $cmd == code* ]]; then
-            $cmd $package
-        else
-            $cmd install $package
-        fi
-    done < "$fn"
-    success "Finished installing $1 packages."
+substep_success "Finished installing Node with FNM."
+
+packages=(
+    "npm"
+    "@antfu/ni"
+    "@bitwarden/cli"
+    "cspell"
+    "easy-sharing"
+    "generator-code"
+    "netlify-cli"
+    "vsce"
+    "wrangler"
+    "yo"
+    "corepack"
+)
+
+for package in "${packages[@]}"; do
+    substep_info "Installing $package..."
+    npm install -g "$package"
+    substep_success "✅ $package installed."
+    if [ "$package" = "corepack" ]; then
+        substep_info "Enabling Corepack..."
+        corepack enable
+        substep_success "🔌 Corepack enabled."
+    fi
 done
+
+success "✅ NodeJS setup complete."
