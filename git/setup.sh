7c73812e33f3b62a1a3a69976e98721638db4a10
Ed Heltzel <ed@rainyday.media>
2023-03-09 08:16:48 -0500

    cleans up setup.sh for git/

--- git/setup.sh
+++ git/setup.sh
@@ -1,24 +1,16 @@
-#! /usr/bin/env sh
-
+#!/bin/bash
 DIR=$(dirname "$0")
 cd "$DIR"
-
 . ../scripts/functions.sh
-
-SOURCE="$(realpath .)"
-DESTINATION="$(realpath ~)"
-
+SOURCE="$(realpath .)";
+DESTINATION="$(realpath ~)";
 info "Setting symlinks for Git."
-
 find . -maxdepth 1 -name ".*" -not -wholename "*DS_Store*" | while read fn; do
-    fn=$(basename $fn)
-    symlink "$SOURCE/$fn" "$DESTINATION/$fn"
+fn=$(basename "$fn")
+symlink "$SOURCE/$fn" "$DESTINATION/$fn"
 done
 substep_success "Successfully created symlinks."
-
 clear_broken_symlinks
-
 substep_success "Setting up Local Git Config for MacDaddy and BigMac"
 localgitconfig
-
 success "Git configured for $(hostname)"
