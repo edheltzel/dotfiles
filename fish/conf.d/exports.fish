d01e21573592d65dd545fb3d76cd515fb72f16c1
Ed Heltzel <ed@rainyday.media>
2023-03-15 12:26:09 -0400

    updates fish - combines _navigation.fish and _aliases.fish

--- fish/conf.d/exports.fish
+++ fish/conf.d/exports.fish
@@ -14,5 +14,4 @@ set -x --global VOL xxx
 
 # Source Multi-function files
 source ~/.config/fish/functions/_aliases.fish
-source ~/.config/fish/functions/_navigation.fish
 source ~/.config/fish/functions/_utils.fish
--- fish/functions/_aliases.fish
+++ fish/functions/_aliases.fish
@@ -94,4 +94,22 @@ function forrepos --description 'Evaluates $argv for all repo folders'
     end
 end
 
+# Navigation
+function ..    ; cd .. ; end
+function ...   ; cd ../.. ; end
+function ....  ; cd ../../.. ; end
+function ..... ; cd ../../../.. ; end
 
+function l; command exa -Flagh --sort name --git --icons --group-directories-first $argv; end
+function ll; command exa -Flagh --git --icons --group-directories-first --sort modified $argv; end
+function la; command exa -Fla --icons; end
+function tree; command exa --tree --icons $argv; end
+# function ls; command exa $argv; end # using native ls for backwards compatibility in some cases
+function cll; command clear; and exa -Flah --icons --sort modified $argv; end
+
+# Often used shortcuts/aliases
+function projects; cd ~/Developer; end
+function dev; cd ~/Developer; end
+function work; cd ~/Developer/work; end
+function dots; cd ~/Developer/dotfiles; end
+function cuts; ~/Projects/dotfiles; and eval $EDITOR .; end
