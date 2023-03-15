a0a7c744862fa4592de1fa480cc9e4c2cae4bb30
Ed Heltzel <ed@rainyday.media>
2023-03-15 12:35:29 -0400

    updates fish - config includes sourcing of variables.fish

--- fish/conf.d/exports.fish
+++ fish/conf.d/exports.fish
@@ -5,13 +5,6 @@ set -g fish_key_bindings fish_default_key_bindings
 # https://github.com/oh-my-fish/plugin-grc/issues/20#issue-296031557
 source /opt/homebrew/etc/grc.fish
 
-# set Visual Studio Code as default editor use code, code-insiders, subl or vim
-set -x --global EDITOR code-insiders
-string match -q "$TERM_PROGRAM" "vscode"
-and . (code-insiders --locate-shell-integration-path fish)
-#set primary volume for workstation
-set -x --global VOL xxx
-
 # Source Multi-function files
 source ~/.config/fish/functions/_aliases.fish
 source ~/.config/fish/functions/_utils.fish
--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 # Custom sourcing of colors, exports, paths, grc, multi-function fish files, etc.
 source ~/.config/fish/conf.d/colors.fish
+source ~/.config/fish/conf.d/variables.fish
 source ~/.config/fish/conf.d/exports.fish
 source ~/.config/fish/conf.d/paths.fish
 
