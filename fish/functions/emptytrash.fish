6f89e154cda2ec84ec3179460f264b1ec439f7b7
Ed Heltzel <ed@rainyday.media>
2023-03-26 10:38:45 -0400

    emptytrash removed

--- fish/functions/emptytrash.fish
+++
@@ -1,8 +0,0 @@
-# Empty the Trash on all mounted volumes and the main HDD.
-# Also, clear Apple’s System Logs to improve shell startup speed.
-# Finally, clear download history from quarantine. https://mths.be/bum
-function emptytrash
-  for files in ~/.Trash/
-      command sudo rm -rdfv $files
-  end
-end
