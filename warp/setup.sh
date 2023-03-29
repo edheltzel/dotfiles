495689141689d879f391de03600f465d595e5a1e
Ed Heltzel <ed@rainyday.media>
2023-03-29 17:29:12 -0400

    warp setup file fix

--- warp/setup.sh
+++ warp/setup.sh
@@ -6,27 +6,29 @@ cd "$DIR"
 . ../scripts/functions.sh
 
 SOURCE="$(realpath .)"
-WARP_PATH="$(realpath ~/.warp)"
+DESTINATION="$(realpath ~)"
+WARP_PATH="$DESTINATION/.warp"
 
 info "Setting up Warp..."
 
 substep_info "Creating Warp folder..."
-mkdir -p $WARP_PATH
+mkdir -p $DESTINATION/.warp
 
-find * -name "*.yaml" | while read fn; do
-    symlink "$SOURCE/$fn" "$WARP_PATH/$fn"
-done
+# find * -name "*.yaml" | while read fn; do
+#     symlink "$SOURCE/$fn" "$WARP_PATH/$fn"
+# done
 
 # Launch Configurations
 find . -name "launch_configurations" | while read fn; do
+    fn=$(basename $fn)
     symlink "$SOURCE/$fn" "$WARP_PATH/$fn"
 done
 
 # Themes
 find . -name "themes" | while read fn; do
+    fn=$(basename $fn)
     symlink "$SOURCE/$fn" "$WARP_PATH/$fn"
 done
-
 clear_broken_symlinks "$WARP_PATH"
 
 success "Finished setting up Warp."
