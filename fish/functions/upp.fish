86fa0b273319c5a806eaeead491265bde9780ab7
Ed Heltzel <ed@rainyday.media>
2023-04-11 09:05:18 -0400

    rewrote the upp - adds comments and better organizes the function for readability.

--- fish/functions/upp.fish
+++ fish/functions/upp.fish
@@ -1,10 +1,30 @@
 # Defined in - @ line 0
 function upp --description 'Updates everything with Topgrade but also prints the default Node version, Update Resets Homebrew, Cleans Homebrew and Z'
+  # Request sudo access
   sudo -v
-  set nodeIcon ' '; set nodeVerb 'Node Version in use: '; set_color green --bold;
-  printf "$nodeIcon"; set_color normal;
-  printf "$nodeVerb"; set_color green --bold; and node --version; set_color white;
-  topgrade --cleanup; set_color white;
-  printf '\n―― '(date "+%H:%M:%S")'- Brew - Cleanup ――――――――――――――――――――――――――――――――――――――――――――――――――――\n'; set_color normal; and brew cleanup --prune=all; set_color white;
-  printf '\n―― '(date "+%H:%M:%S")'- pnpm yarn - Corepack  ――――――――――――――――――――――――――――――――――――――――――――――――――――――\n'; set_color normal; and corepack prepare pnpm@latest --activate; and corepack prepare yarn@stable --activate;
+
+  # Define variables for Node output
+  set -l nodeIcon ' '
+  set -l nodeVerb 'Node Version in use: '
+
+  # Print Node version
+  printf "%s" "$nodeIcon"
+  set_color green --bold
+  printf "%s" "$nodeVerb"
+  set_color normal
+  node --version
+  set_color white
+
+  # Run topgrade and clean up Homebrew
+  topgrade --cleanup
+  printf '\n―― %s - Brew - Cleanup ―――――――――――――――――――――――――――――――――――――――――――――――――――\n' (date "+%H:%M:%S")
+  set_color normal
+  brew cleanup --prune=all
+  set_color white
+
+  # Prepare pnpm and yarn with Corepack
+  printf '\n―― %s - pnpm yarn - Corepack  ――――――――――――――――――――――――――――――――――――――――――――――――――――\n' (date "+%H:%M:%S")
+  set_color normal
+  corepack prepare pnpm@latest --activate
+  corepack prepare yarn@stable --activate
 end
