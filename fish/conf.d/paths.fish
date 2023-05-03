11f5e318eb843aa3d9e2229dbbe458d4496b6ec7
Ed Heltzel <ed@rainyday.media>
2023-05-03 10:00:59 -0400

    fish path for docker

--- fish/conf.d/paths.fish
+++ fish/conf.d/paths.fish
@@ -45,5 +45,5 @@ test -f '/Users/ed/Library/Preferences/netlify/helper/path.fish.inc' && source '
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/__tabtab.fish ]; and . ~/.config/tabtab/__tabtab.fish; or true
 
-# Docker
-set -g fish_user_paths $HOME/.docker/bin $fish_user_paths
+# # Docker
+# set -g fish_user_paths $HOME/.docker/bin $fish_user_paths
