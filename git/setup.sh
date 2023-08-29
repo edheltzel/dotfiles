29c39204158eeeee1530d01627d9e57a4627bc38
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-08-29 11:30:24 -0400

    better symlinking of local gitconfig based on Computer name/hostname

--- git/setup.sh
+++ git/setup.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 DIR=$(dirname "$0")
 cd "$DIR"
+
 . ../scripts/functions.sh
-SOURCE="$(realpath .)";
-DESTINATION="$(realpath ~)";
+
+SOURCE="$(realpath .)"
+DESTINATION="$(realpath ~)"
+
+# Get computer name
+computer_name=$(scutil --get ComputerName)
+
+if [ "$computer_name" = "BigMac" ]; then
+  symlink "$SOURCE/gitconfig-bigmac.local" "$DESTINATION/.gitconfig.local"
+elif [ "$computer_name" = "MacDaddy" ]; then
+  symlink "$SOURCE/gitconfig-macdaddy.local" "$DESTINATION/.gitconfig.local"
+else
+  echo "Unknown computer name: $computer_name"
+  exit 1
+fi
+
 info "Setting symlinks for Git."
+
 find . -maxdepth 1 -name ".*" -not -wholename "*DS_Store*" | while read fn; do
-fn=$(basename "$fn")
-symlink "$SOURCE/$fn" "$DESTINATION/$fn"
+  fn=$(basename "$fn")
+  symlink "$SOURCE/$fn" "$DESTINATION/$fn"
 done
+
 substep_success "Successfully created symlinks."
 clear_broken_symlinks
-substep_success "Setting up Local Git Config for MacDaddy and BigMac"
-localgitconfig
-success "Git configured for $(hostname)"
