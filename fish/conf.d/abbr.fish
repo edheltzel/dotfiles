51ee2b43c702316f82912a50e6e21c3c06942854
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-09-13 16:03:36 -0400

    changing cls abbr

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -4,7 +4,7 @@ if status is-interactive
     abbr --add binfo 'brew info'
     abbr --add brews 'brew leaves'
     abbr --add casks 'brew list --cask'
-    abbr --add cls 'clear'
+    abbr --add cls '~/Library/CloudStorage/Dropbox-RDM/Clients'
     abbr --add co 'code .'
     abbr --add con 'code -n .'
     abbr --add config '~/.config/'
