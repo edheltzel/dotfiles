495bbd4fe50706a05e4f9d1b03d6dc889dc1d9e1
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-10-22 08:53:15 -0400

    formatting

--- fish/functions/_utils.fish
+++ fish/functions/_utils.fish
@@ -1,5 +1,9 @@
 # Utilities for checking terminal color support, and printing color pallete
-function colormap; $HOME/.config/utils/color-map.sh; end
+function colormap
+    $HOME/.config/utils/color-map.sh
+end
 
 # Matrix-style screensaver for your terminal - for fun
-function matrix; $HOME/.config/utils/matrix.sh; end
+function matrix
+    $HOME/.config/utils/matrix.sh
+end
