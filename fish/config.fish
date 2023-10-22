693883f959ae3b7638294c7f610654d1beffcbfe
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-10-22 10:01:11 -0400

    update note

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 # Custom sourcing of colors, exports, paths, grc, multi-function fish files, etc.
-source ~/.config/fish/conf.d/colors.fish # this could be obsoleted by starship & exa
+source ~/.config/fish/conf.d/colors.fish # this could be obsoleted by starship & eza
 source ~/.config/fish/conf.d/variables.fish
 source ~/.config/fish/conf.d/exports.fish
 source ~/.config/fish/conf.d/paths.fish
