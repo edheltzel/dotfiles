4752b32029552205168ff6934639e29d36dcc306
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-11-01 07:48:25 -0400

    Fish: abbrivations update

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-    abbr --add atm 'neofetch'
+    abbr --add atm neofetch
     abbr --add bi 'brew install '
     abbr --add binfo 'brew info'
     abbr --add brews 'brew list'
@@ -15,18 +15,20 @@ if status is-interactive
     abbr --add ghw 'gh repo view --web'
     abbr --add ghpr 'gh pr create -a "@me" --fill'
     abbr --add ghm --set-cursor 'gh pr merge % --squash'
+    abbr --add ghr --set-cursor 'gh release create v% --generate-notes --latest'
     abbr --add goo 'cd ~/.go/'
     abbr --add npms 'npm list -g --depth=0'
     abbr --add pns 'pnpm list -g'
     abbr --add pnpms 'pnpm list -g'
     abbr --add buns 'bun pm ls -g'
     abbr --add siz 'du -khsc'
-    abbr --add sp 'speedtest'
+    abbr --add sp speedtest
     abbr --add suckit 'wget -mkEpnp url_here'
     abbr --add link 'ln -s'
     abbr --add symlink 'ln -s'
     abbr --add amux 'tmux at -t base'
     abbr --add tkill 'tmux kill-session -t'
     abbr --add nmux 'tmux new -s "base"'
+    abbr --add vim nvim
     abbr --add zpurge 'z --clean'
 end
