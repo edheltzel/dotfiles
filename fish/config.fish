dcc3e0fea7facda5d77734ddffc40e0e912f6dbb
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-11-01 16:15:31 -0400

    🐚 Prompt Update - adds oh-my-posh with starter theme - updates starship prompt theme

--- fish/config.fish
+++ fish/config.fish
@@ -4,9 +4,10 @@ source ~/.config/fish/conf.d/variables.fish
 source ~/.config/fish/conf.d/exports.fish
 source ~/.config/fish/conf.d/paths.fish
 source ~/.config/fish/conf.d/abbr.fish
-# Prompt - Starship
-starship init fish | source
 
-# tabtab source for packages
-# uninstall by removing these lines
+# Prompt - Starship or Oh My Posh
+starship init fish | source
+# oh-my-posh init fish --config 'https://raw.githubusercontent.com/JanDeDobbeleer/dotfiles/main/dot_jandedobbeleer.omp.json' | source
+
+# tabtab source for packages – to uninstall by removing these lines
 [ -f ~/.config/tabtab/__tabtab.fish ]; and . ~/.config/tabtab/__tabtab.fish; or true
