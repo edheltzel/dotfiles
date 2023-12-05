fd1d34e91c36ed864f87c7261fe9b60c51ae6287
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-12-05 11:53:20 -0500

    fix broken node icon in `upp`

--- fish/functions/upp.fish
+++ fish/functions/upp.fish
@@ -1,30 +1,30 @@
 # Defined in - @ line 0
 function upp --description 'Updates everything with Topgrade but also prints the default Node version, Update Resets Homebrew, Cleans Homebrew and Z'
-  # Request sudo access
-  # sudo -v
+    # Request sudo access
+    # sudo -v
 
-  # Define variables for Node output
-  set -l nodeIcon ' '
-  set -l nodeVerb 'Node Version in use: '
+    # Define variables for Node output
+    set -l nodeIcon '󰎙 '
+    set -l nodeVerb 'Node Version in use: '
 
-  # Print Node version
-  printf "%s" "$nodeIcon"
-  set_color green --bold
-  printf "%s" "$nodeVerb"
-  set_color normal
-  node --version
-  set_color white
+    # Print Node version
+    printf "%s" "$nodeIcon"
+    set_color green --bold
+    printf "%s" "$nodeVerb"
+    set_color normal
+    node --version
+    set_color white
 
-  # Run topgrade and clean up Homebrew
-  topgrade
-  printf '\n―― %s - Brew - Cleanup ―――――――――――――――――――――――――――――――――――――――――――――――――――\n' (date "+%H:%M:%S")
-  set_color normal
-  brew cleanup --prune=all
-  set_color white
+    # Run topgrade and clean up Homebrew
+    topgrade
+    printf '\n―― %s - Brew - Cleanup ―――――――――――――――――――――――――――――――――――――――――――――――――――\n' (date "+%H:%M:%S")
+    set_color normal
+    brew cleanup --prune=all
+    set_color white
 
-  # Prepare pnpm and yarn with Corepack
-  printf '\n―― %s - pnpm - Corepack  ――――――――――――――――――――――――――――――――――――――――――――――――――――\n' (date "+%H:%M:%S")
-  set_color normal
-  corepack prepare pnpm@latest --activate
-  pnpm update -g --latest
+    # Prepare pnpm and yarn with Corepack
+    printf '\n―― %s - pnpm - Corepack  ――――――――――――――――――――――――――――――――――――――――――――――――――――\n' (date "+%H:%M:%S")
+    set_color normal
+    corepack prepare pnpm@latest --activate
+    pnpm update -g --latest
 end
