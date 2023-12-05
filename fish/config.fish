02587cb8154615e2f8c842910b2fa9c66130df20
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-12-05 15:30:31 -0500

    update tabtab for pnpm

--- fish/config.fish
+++ fish/config.fish
@@ -8,5 +8,6 @@ source ~/.config/fish/conf.d/abbr.fish
 # Prompt - Starship
 starship init fish | source
 
-# tabtab source for packages – to uninstall by removing these lines
-[ -f ~/.config/tabtab/__tabtab.fish ]; and . ~/.config/tabtab/__tabtab.fish; or true
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
