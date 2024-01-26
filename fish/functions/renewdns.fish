3a0a173efab82121cdb26d77c886f4c90fb8f516
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-01-26 16:12:04 -0500

    Adds 2 new functions to fish:
    - renewdns function: DNS lease renewal
    - killws function: kills the process for WindowServer

--- fish/functions/renewdns.fish
+++ fish/functions/renewdns.fish
@@ -1,12 +1,9 @@
 function renewdns --description 'Renews the DNS lease by disabling and re-enabling the network adapter on macOS'
-  # Request the user to run the script with sudo if not already running as root
-  if not [ (id -u) -eq 0 ]
-      echo "Please run this script with sudo."
-      return 1
-  end
-
-  # Now that we're running with elevated privileges, we can run the commands without sudo
-  ifconfig en0 down
-  sleep 1  # Optional: give the network interface a moment to go down
-  ifconfig en0 up
+    set sudo_pass (sudo -p "Password: " -S echo)
+    set result (echo $sudo_pass | sudo -S ifconfig en0 down; and sleep 1; echo $sudo_pass | sudo -S ifconfig en0 up)
+    if test $status -eq 0
+        echo "DNS lease successfully renewed"
+    else
+        echo "Failed to renew DNS lease"
+    end
 end
