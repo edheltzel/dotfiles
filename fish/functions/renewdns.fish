a35d86c11bf2f5b73928043785de9fac9a04d1e3
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-01-28 07:06:30 -0500

    windowserver and flashEthernet functions

--- fish/functions/renewdns.fish
+++
@@ -1,9 +0,0 @@
-function renewdns --description 'Renews the DNS lease by disabling and re-enabling the network adapter on macOS'
-    set sudo_pass (sudo -p "Password: " -S echo)
-    set result (echo $sudo_pass | sudo -S ifconfig en0 down; and sleep 1; echo $sudo_pass | sudo -S ifconfig en0 up)
-    if test $status -eq 0
-        echo "DNS lease successfully renewed"
-    else
-        echo "Failed to renew DNS lease"
-    end
-end
