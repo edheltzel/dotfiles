da8b0dec3076cecf390a3a613c8a98cc087d9bd3
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-01-28 18:14:17 -0500

    adding wait to `$EDITOR`

--- fish/conf.d/variables.fish
+++ fish/conf.d/variables.fish
@@ -1,5 +1,5 @@
 # set Visual Studio Code as default editor use code, code-insiders, subl or vim
-set -x --global EDITOR code-insiders
+set -x --global EDITOR code-insiders -w
 string match -q "$TERM_PROGRAM" "vscode"
 and . (code --locate-shell-integration-path fish)
 #set primary volume for workstation
