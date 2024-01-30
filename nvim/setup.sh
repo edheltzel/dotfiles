0f6cfc851d1cb88f4c86c827f82858c0b400b773
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-01-30 14:07:31 -0500

    making nvim a stow package again

---
+++ nvim/setup.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env sh
+
+DIR=$(dirname "$0")
+cd "$DIR"
+
+. ../scripts/functions.sh
+
+CONFIG_PATH="$(realpath ~/.config)"
+NVM_DIR="$(realpath ~/.config/nvim)"
+VIM_DISTRO="LazyVim"
+
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+
+install_nvm() {
+  nvm_repo='https://github.com/LazyVim/starter.git' # Corrected this line
+  if [ -d "$NVM_DIR" ]; then # Already installed, update
+    cd $NVM_DIR && git pull # Corrected this line
+  else # Not yet installed, prompt user to confirm before proceeding
+    if read -p "$(echo -e "${YELLOW}Do you want to clone ${VIM_DISTRO}? (y/n): ${NC}")"; then
+      echo -e "Installing..."
+      cd $CONFIG_PATH && git clone $nvm_repo nvim
+    else
+      echo -e "Aborting..."
+      return
+    fi
+  fi
+}
+
+# Call the install_nvm function
+install_nvm
+
+info "Setting symlinks for ${VIM_DISTRO}."
+substep_success "nvim folder created."
+success "Nvim with ${VIM_DISTRO} is ready. For more info: https://www.lazyvim.org/"
