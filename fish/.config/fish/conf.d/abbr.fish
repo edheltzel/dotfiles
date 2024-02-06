8a7be50b44154105fb07ba2f9752d35214702806
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-02-06 15:17:36 -0500

    fix duplication with ghpr

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -15,7 +15,7 @@ if status is-interactive
     abbr --add gems 'gem list'
     abbr --add gg 'go get GITHUB_URL'
     abbr --add ghw 'gh repo view --web'
-    abbr --add gpr 'gh pr create -a "@me" --fill'
+    abbr --add ghpr 'gh pr create -a "@me" --fill'
     abbr --add ghm --set-cursor 'gh pr merge % --merge'
     abbr --add ghr --set-cursor 'gh release create v% --generate-notes --latest'
     abbr --add goo 'cd ~/.go/'
