5db00a78c758d82d7a942c5824d0983616cfabb6
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-02-09 10:40:18 -0500

    adds dua-cli back to the mix to replace du

--- fish/.config/fish/functions/_aliases.fish
+++ fish/.config/fish/functions/_aliases.fish
@@ -33,6 +33,7 @@ alias code code-insiders
 alias top btm
 alias bottom btm
 alias serve miniserve #start a simple http server
+alias du dua
 
 # function tldrf --description 'tldr search and preview with fzf'
 alias tldrf 'tldr --list | fzf --preview "tldr {1} --color=always" --preview-window=right,70% | xargs tldr'
