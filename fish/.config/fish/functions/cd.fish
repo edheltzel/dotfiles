dc6782baaf2e69d2a584b092f38ba73cbd42c566
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-02-10 08:18:56 -0500

    preventing error when navigating to a directory

--- fish/.config/fish/functions/cd.fish
+++ fish/.config/fish/functions/cd.fish
@@ -1,7 +1,7 @@
 function cd --description 'list files in the current directory after changing to it'
     if count $argv >/dev/null
         # prevents recurse infinitely by using built-in cd
-        builtin cd "$argv"; and eza -lagh --sort=modified--git --icons --group-directories-first
+        builtin cd "$argv"; and eza -lagh --sort=name --git --icons --group-directories-first
     else
         builtin cd ~; and eza -lagh --sort=name --git --icons --group-directories-first
     end
