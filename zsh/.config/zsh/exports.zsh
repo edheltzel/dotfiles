5d5eecdcb47ba925622e26c88acbab9ac6f50759
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-02-13 11:54:32 -0500

    Update zsh exports with comments

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -1,6 +1,6 @@
 #!/bin/sh
-eval "$(/opt/homebrew/bin/brew shellenv)"
-eval "$(starship init zsh)"
-eval "$(fnm env --use-on-cd)"
-eval "$(zoxide init zsh)"
+eval "$(/opt/homebrew/bin/brew shellenv)" # homebrew
+eval "$(starship init zsh)"               # starship prompt
+eval "$(fnm env --use-on-cd)"             # fnm node version manager
+eval "$(zoxide init zsh)"                 # zoxide directory jump
 # eval "`pip completion --zsh`"
