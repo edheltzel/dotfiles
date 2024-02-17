af1dbb1aa419ba4ae75a9772515c5cae03104f0f
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-02-17 08:41:55 -0500

    removes the `.git/` dir after fresh clone/install of lazyvim

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -12,12 +12,11 @@ VIM_DISTRO="LazyVim"
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
-
 install_nvm() {
   nvm_repo='https://github.com/LazyVim/starter.git' # Corrected this line
-  if [ -d "$NVM_DIR" ]; then # Already installed, update
-    cd $NVM_DIR && git pull # Corrected this line
-  else # Not yet installed, prompt user to confirm before proceeding
+  if [ -d "$NVM_DIR" ]; then                        # Already installed, update
+    cd $NVM_DIR && git pull                         # Corrected this line
+  else                                              # Not yet installed, prompt user to confirm before proceeding
     if read -p "$(echo -e "${YELLOW}Do you want to clone ${VIM_DISTRO}? (y/n): ${NC}")"; then
       echo -e "Installing..."
       cd $CONFIG_PATH && git clone $nvm_repo nvim
@@ -30,6 +29,9 @@ install_nvm() {
 
 # Call the install_nvm function
 install_nvm
+
+# delete the .git folder after install_nvm runs
+rm -rf $NVM_DIR/.git
 
 info "Setting symlinks for ${VIM_DISTRO}."
 substep_success "nvim folder created."
