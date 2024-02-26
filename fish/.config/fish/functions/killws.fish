5a0b2f7a1f814ecd2797c3db96f415be1fb543a2
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-02-26 10:47:51 -0500

    formatting

--- fish/.config/fish/functions/killws.fish
+++ fish/.config/fish/functions/killws.fish
@@ -1,4 +1,4 @@
 # This will log you out - macOS has a bug with multiple displays where the WindowServer will consume CPU and/or Memory. It is annoying.'
 function killws --description 'Restart the WindowServer on macOS'
-    sudo killall -HUP WindowServer;
+    sudo killall -HUP WindowServer
 end
