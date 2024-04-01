912aae543f75ee15a71cc06cce08305ebdd75088
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-04-01 07:13:55 -0400

    Fish config update
    includes iterm integration with fish for iterm2 features

--- fish/.config/fish/conf.d/exports.fish
+++ fish/.config/fish/conf.d/exports.fish
@@ -8,3 +8,6 @@ source /opt/homebrew/etc/grc.fish
 source ~/.config/fish/functions/_aliases.fish
 source ~/.config/fish/functions/_utils.fish
 source ~/.config/fish/functions/_backup_restore.fish
+
+# Terminal integration
+source ~/.config/fish/conf.d/iterm2_shell_integration.fish
