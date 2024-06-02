4afc2cabc1cf4c9fdc67852b2b4efcff1e0bf487
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-06-02 12:11:31 -0400

    using AstroNvim distro

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    -- add more things to the ensure_installed table protecting against community packs modifying it
+    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+      "lua",
+      "vim",
+      -- add more arguments for adding more treesitter parsers
+    })
+  end,
+}
