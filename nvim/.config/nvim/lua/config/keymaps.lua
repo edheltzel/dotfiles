84ed6e64606c30b6f7fb1e2fa4a448e095b0795f
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-06-02 12:10:34 -0400

    removes lazyvim distro

--- nvim/.config/nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
--- vim.keymap.set("n", "Y", "y$", { desc = "Yank to end of line" })
-vim.keymap.set("n", "<Leader>fs", ":w<CR>", { desc = "Save file" })
