f1d360aa996f899325406eb83b4e7efbdb12d027
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-10-04 11:54:56 -0400

    adds wezterm and update neovim configs
    using wezterm might be a good option to replace kitty, it will at least
    open in the center of the screen just not on my primary monitor. 🙄
    
    need to update key bindings to be more like iterm
    
    neovim now uses solarized osaka theme

---
+++ config/.config/wezterm/config.lua
@@ -0,0 +1,69 @@
+local wezterm = require("wezterm")
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config = {
+  default_cursor_style = "BlinkingBlock",
+  automatically_reload_config = true,
+  window_close_confirmation = "NeverPrompt",
+  adjust_window_size_when_changing_font_size = false,
+  window_decorations = "RESIZE",
+  check_for_updates = false,
+  use_fancy_tab_bar = true,
+  tab_bar_at_bottom = true,
+  font_size = 17,
+  font = wezterm.font("Cascadia Code NF"),
+  enable_tab_bar = false,
+  window_padding = {
+    left = 3,
+    right = 3,
+    top = 0,
+    bottom = 0,
+  },
+  -- from: https://akos.ma/blog/adopting-wezterm/
+  hyperlink_rules = {
+    -- Matches: a URL in parens: (URL)
+    {
+      regex = "\\((\\w+://\\S+)\\)",
+      format = "$1",
+      highlight = 1,
+    },
+    -- Matches: a URL in brackets: [URL]
+    {
+      regex = "\\[(\\w+://\\S+)\\]",
+      format = "$1",
+      highlight = 1,
+    },
+    -- Matches: a URL in curly braces: {URL}
+    {
+      regex = "\\{(\\w+://\\S+)\\}",
+      format = "$1",
+      highlight = 1,
+    },
+    -- Matches: a URL in angle brackets: <URL>
+    {
+      regex = "<(\\w+://\\S+)>",
+      format = "$1",
+      highlight = 1,
+    },
+    -- Then handle URLs not wrapped in brackets
+    {
+      -- Before
+      --regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
+      --format = '$0',
+      -- After
+      regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
+      format = "$1",
+      highlight = 1,
+    },
+    -- implicit mailto link
+    {
+      regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+      format = "mailto:$0",
+    },
+  },
+}
+return config
---
+++ config/.config/wezterm/wezterm.lua
@@ -0,0 +1,6 @@
+local wezterm = require("wezterm")
+local config = require("config")
+
+config.color_scheme = "Solarized Dark Higher Contrast"
+
+return config
