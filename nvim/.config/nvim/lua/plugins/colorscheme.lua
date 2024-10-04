b3efe4c691fec6c90ca6d1017a77d4c4abb9c2e7
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-10-04 09:03:10 -0400

    adds Neovim Solarized Osaka
    sets kitty to use solarzied dark high contrast

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,21 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = true,
+{
+  "craftzdog/solarized-osaka.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = function(_, opts)
+    opts.transparent = true
+    opts.italic_comments = true
+    opts.styles={
+      sidebar = "transparent",
+      floats = "dark",
+    }
+  end,
+ },
+ {
+  "LazyVim/LazyVim",
   opts = {
-    style = "night",
+    colorscheme = "solarized-osaka",
   },
+ },
 }
