26dd74ebd1e4f0c347e3d5be2086d16617629362
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-10-05 18:08:54 -0400

    nvim plugin updates

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,21 @@
 return {
-{
-  "craftzdog/solarized-osaka.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = function(_, opts)
-    opts.transparent = true
-    opts.italic_comments = true
-    opts.styles={
-      sidebar = "transparent",
-      floats = "dark",
-    }
-  end,
- },
- {
-  "LazyVim/LazyVim",
-  opts = {
-    colorscheme = "solarized-osaka",
-  },
- },
+	{
+		"craftzdog/solarized-osaka.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = function(_, opts)
+			opts.transparent = true
+			opts.italic_comments = true
+			opts.styles = {
+				sidebar = "transparent",
+				floats = "dark",
+			}
+		end,
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "solarized-osaka",
+		},
+	},
 }
--- nvim/.config/nvim/lua/plugins/supermaven.lua
+++ nvim/.config/nvim/lua/plugins/supermaven.lua
@@ -4,9 +4,18 @@ return {
 		config = function()
 			require("supermaven-nvim").setup({
 				keymaps = {
-					accept_selection = "<Tab>",
-					accept_word = "<C-j>",
+					accept_suggestion = "<C-j>",
+					clear_suggestion = "<C-]>",
+					accept_word = "<C-k>",
 				},
+				ignore_filetypes = { cpp = true },
+				color = {
+					suggestion_color = "#586E75",
+					cterm = 244,
+				},
+				log_level = "info", -- set to "off" to disable logging completely
+				disable_inline_completion = false, -- disables inline completion for use with cmp
+				disable_keymaps = false,
 			})
 		end,
 	},
