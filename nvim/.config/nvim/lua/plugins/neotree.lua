fee9ee5b1f9b7e4d9f6853ccc1cf6a8c058bf32b
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-10-21 12:45:24 -0400

    neotree on the right side in neovim
    slowly getting neovim to match my vscode setup - plan on switching in
    the future.

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,22 +1,60 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          show_hidden_count = true,
-          hide_dotfiles = false,
-          hide_gitignored = true,
-          hide_by_name = {
-            ".git",
-            "thumbs.db",
-          },
-          never_show = {
-            ".DS_Store",
-          },
-        },
-      },
-    },
-  },
+	-- Changed windows position to right, excluded some directories
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			close_if_last_window = true,
+			window = {
+				position = "right",
+				width = 40,
+			},
+			default_component_configs = {
+				file_size = {
+					enabled = true,
+					required_width = 64, -- min width of window required to show this column
+				},
+				type = {
+					enabled = true,
+					required_width = 122, -- min width of window required to show this column
+				},
+				last_modified = {
+					enabled = true,
+					required_width = 88, -- min width of window required to show this column
+				},
+				created = {
+					enabled = true,
+					required_width = 110, -- min width of window required to show this column
+				},
+				symlink_target = {
+					enabled = false,
+				},
+			},
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = true,
+					hide_hidden = true, -- only works on Windows for hidden files/directories
+					hide_by_name = {
+						"node_modules",
+						".git",
+					},
+					hide_by_pattern = { -- uses glob style patterns
+						--"*.meta",
+						--"*/src/*/tsconfig.json",
+					},
+					always_show = { -- remains visible even if other settings would normally hide it
+						--".gitignored",
+					},
+					never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+						".DS_Store",
+						--"thumbs.db"
+					},
+					never_show_by_pattern = { -- uses glob style patterns
+						--".null-ls_*",
+					},
+				},
+			},
+		},
+	},
 }
