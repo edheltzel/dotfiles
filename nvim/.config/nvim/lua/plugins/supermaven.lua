a46068413fe2416af42e907605f3b07466b2e2ef
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-10-21 10:55:42 -0400

    neovim update - reconfigured supermaven plugin

--- nvim/.config/nvim/lua/plugins/supermaven.lua
+++ nvim/.config/nvim/lua/plugins/supermaven.lua
@@ -1,22 +1,22 @@
 return {
-	{
-		"supermaven-inc/supermaven-nvim",
-		config = function()
-			require("supermaven-nvim").setup({
-				keymaps = {
-					accept_suggestion = "<C-j>",
-					clear_suggestion = "<C-]>",
-					accept_word = "<C-k>",
-				},
-				ignore_filetypes = { cpp = true },
-				color = {
-					suggestion_color = "#586E75",
-					cterm = 244,
-				},
-				log_level = "info", -- set to "off" to disable logging completely
-				disable_inline_completion = false, -- disables inline completion for use with cmp
-				disable_keymaps = false,
-			})
-		end,
-	},
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<C-j>",
+          clear_suggestion = "<C-]>",
+          accept_word = "<C-k>",
+        },
+        ignore_filetypes = { cpp = true },
+        color = {
+          suggestion_color = "#ffffff",
+          cterm = 244,
+        },
+        log_level = "info", -- set to "off" to disable logging completely
+        disable_inline_completion = false, -- disables inline completion for use with cmp
+        disable_keymaps = false,
+      })
+    end,
+  },
 }
