75d1d0ce3915e2ce22644cef731b1e76ed59324b
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-10-24 12:05:34 -0400

    nvim plugin update - supermaven
    fixed the file, forgot to return - which was causing issues, even
    through supermaven would still load

--- nvim/.config/nvim/lua/plugins/supermaven.lua
+++ nvim/.config/nvim/lua/plugins/supermaven.lua
@@ -1,25 +1,25 @@
-require("lazy").setup({
+return{
   {
     "supermaven-inc/supermaven-nvim",
     config = function()
       require("supermaven-nvim").setup({
-        keymaps = {
-          accept_suggestion = "<Tab>",
-          clear_suggestion = "<C-]>",
-          accept_word = "<C-j>",
-        },
-        ignore_filetype = { cpp = true },
-        color = {
-          suggestion_color = "#292D43",
-          cterm = 244,
-        },
-        log_level = "info",
-        disable_inline_completion = false,
-        disable_keymaps = false,
-        condition = function()
-          return false
-        end,
+       kemaps = {
+        accept_suggestion = "<Tab>",
+        clear_suggestion = "<C-]>",
+        accpet_word = "<C-j>"
+       },
+       ignore_filetypes = { cpp = true },
+       color = {
+        suggestion_color = "#292D43",
+        cterm = 244,
+       },
+       log_level = "info", -- set to "off" to disable logging completely
+       disable_inline_completion = false, -- disables inline completion for use with cmp
+       disable_keymaps = false,
+       condition = function()
+        return false
+       end,
       })
     end,
   },
-}, {})
+}
