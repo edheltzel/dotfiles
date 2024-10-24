5a7c2ef74f6000f2ef4919ad0359662c1a378f67
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-10-24 12:06:14 -0400

    nvim update
    new color theme eldritch - its like tokyo-night, capputccin and dracula
    merged

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,35 @@
 return {
-  -- {
-  -- 	"craftzdog/solarized-osaka.nvim",
-  -- 	lazy = false,
-  -- 	priority = 1000,
-  -- 	opts = function(_, opts)
-  -- 		opts.transparent = true
-  -- 		opts.italic_comments = true
-  -- 		opts.styles = {
-  -- 			sidebar = "transparent",
-  -- 			floats = "dark",
-  -- 		}
-  -- 	end,
-  -- },
+  {
+    "eldritch-theme/eldritch.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
   {
     "folke/tokyonight.nvim",
     lazy = true,
     priority = 1000,
     opts = {
       style = "night",
+    },
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = function(_, opts)
+      opts.transparent = true
+      opts.italic_comments = true
+      opts.styles = {
+        sidebar = "transparent",
+        floats = "dark",
+      }
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "eldritch",
     },
   },
 }
