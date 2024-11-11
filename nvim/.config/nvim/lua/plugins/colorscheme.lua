06a22a6b0bc5e12b0af7b4f3710ef272698cc42c
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-11-11 16:06:55 -0500

    removes unused color theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,33 +5,11 @@ return {
     priority = 1000,
     opts = {},
   },
-  {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = function(_, opts)
-      opts.transparent = true
-      opts.italic_comments = true
-      opts.borderless_telescope = false
-    end,
-  },
+  -- set colortheme
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "eldritch",
     },
-  },
-
-  -- modicator (auto color line number based on vim mode)
-  {
-    "mawkler/modicator.nvim",
-    dependencies = "scottmckendry/cyberdream.nvim",
-    init = function()
-      -- These are required for Modicator to work
-      vim.o.cursorline = false
-      vim.o.number = true
-      vim.o.termguicolors = true
-    end,
-    opts = {},
   },
 }
