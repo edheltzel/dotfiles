a088f7c76096dab838af25ece45f9fa7e3423ca8
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-01-02 09:56:33 -0500

    wezterm config update

--- config/.config/wezterm/wezterm.lua
+++ config/.config/wezterm/wezterm.lua
@@ -35,14 +35,14 @@ config.color_scheme = "Eldritch"
 
 -- Nerdfonts are baked into Wezterm
 config.font = wezterm.font_with_fallback({
-  { family = "Lilex", weight = "Regular", scale = 1.45 },
+  { family = "Lilex",      weight = "Regular", scale = 1.45 },
   { family = "CommitMono", weight = "Regular", scale = 1.45 },
 })
 
 -- Window Config
 config.max_fps = 240
-config.macos_window_background_blur = 30
-config.window_background_opacity = 0.95
+-- config.macos_window_background_blur = 30
+-- config.window_background_opacity = 0.95
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt" -- AlwaysPrompt | NeverPrompt
 config.scrollback_lines = 10000
