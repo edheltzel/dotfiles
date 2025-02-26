6930dd331fc3e7d0fae602f419621afbafc474a3
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-02-26 09:22:05 -0500

    orbstack cli support

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,3 +21,7 @@ enable_transience
 
 # Added by Windsurf
 fish_add_path /Users/ed/.codeium/windsurf/bin
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
