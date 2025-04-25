805d781d60b1fefceea0d0d1da3af89a88a95099
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-04-25 18:10:31 -0400

    setting up lazyvim
    adds:
    - eldrich theme back
    - neotree config
    - supermaven for autocomplete on steroids

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "eldritch-theme/eldritch.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  -- set colortheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "eldritch",
+    },
+  },
+}
