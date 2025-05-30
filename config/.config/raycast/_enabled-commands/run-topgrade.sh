04d6bba49cacaebcde298e647ce264ed404a23b9
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-05-30 09:45:26 -0400

    🛠️ Update to raycast update script
    - updates the script to use the quick access panel instead of a new
    terminal window. This is dependent on Kitty being installed.

--- config/.config/raycast/_enabled-commands/run-topgrade.sh
+++ config/.config/raycast/_enabled-commands/run-topgrade.sh
@@ -15,8 +15,15 @@
 # @raycast.author edheltzel
 # @raycast.authorURL https://raycast.com/edheltzel
 
-open -a kitty
-sleep 1 # Wait for terminal to open
-osascript -e 'tell application "kitty" to activate'
-osascript -e 'tell application "System Events" to keystroke "topgrade"'
+# Application Mode: this will open a kitty window and run topgrade command
+# open -a kitty
+# sleep 1 # Wait for terminal to open
+# osascript -e 'tell application "kitty" to activate'
+# osascript -e 'tell application "System Events" to keystroke "topgrade"'
+# osascript -e 'tell application "System Events" to key code 36' # Press Enter
+
+# Quick Access Mode: this will open a kitty window and run topgrade command
+osascript -e 'tell application "System Events" to keystroke "t" using {control down, option down, command down}'
+sleep 1 # Wait for quick access top open
+osascript -e 'tell application "System Events" to keystroke "topgrade"' # Run topgrade
 osascript -e 'tell application "System Events" to key code 36' # Press Enter
