292e04fb0c2ae5e550d66a225ab3dc17cbef96d1
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-07-01 10:08:41 -0400

    backup of original nvim backup

---
+++ nvim/.config/nvim.lazyvim.bak/lua/plugins/snacks.lua
@@ -0,0 +1,53 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            layout = {
+              layout = { position = "right" },
+            },
+          },
+          files = {
+            hidden = true,
+            ignored = true,
+            exclude = {
+              ".DS_Store",
+              "**/.git/*",
+              "**/node_modules/*",
+            },
+          },
+        },
+        hidden = true,
+        ignored = true,
+      },
+      dashboard = {
+        preset = {
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            {
+              icon = " ",
+              key = "c",
+              desc = "Config",
+              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+            },
+            {
+              icon = " ",
+              key = "d",
+              desc = "Dotfiles",
+              action = ":cd ~/.dotfiles | :e .",
+            },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+        },
+      },
+    },
+  },
+}
