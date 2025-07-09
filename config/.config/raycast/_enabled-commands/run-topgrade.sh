e0f94be93ef7ff379ab1935cf0ce7d807351c307
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-07-09 14:35:29 -0400

    run-topgrade update
    -ghostty is the terminal

--- config/.config/raycast/_enabled-commands/run-topgrade.sh
+++ config/.config/raycast/_enabled-commands/run-topgrade.sh
@@ -23,7 +23,12 @@
 # osascript -e 'tell application "System Events" to key code 36' # Press Enter
 
 # Quick Access Mode: this will open a kitty window and run topgrade command
+open -a ghostty
+sleep 0.25 # Wait for quick access top open
+# Close any window that might have opened
+# osascript -e 'tell application "Ghostty" to close every window'
+# sleep 0.25
 osascript -e 'tell application "System Events" to keystroke "t" using {control down, option down, command down}'
-sleep 1 # Wait for quick access top open
+sleep 0.25                                                              # Wait for quick access top open
 osascript -e 'tell application "System Events" to keystroke "topgrade"' # Run topgrade
-osascript -e 'tell application "System Events" to key code 36' # Press Enter
+osascript -e 'tell application "System Events" to key code 36'          # Press Enter
