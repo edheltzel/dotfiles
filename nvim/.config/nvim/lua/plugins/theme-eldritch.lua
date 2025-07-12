6a57750fc50513bd838f9166d9c393cd3bd3d1ff
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-07-11 23:15:40 -0400

    🌀Neovim update: trying out rose-pine

--- nvim/.config/nvim/lua/plugins/theme-eldritch.lua
+++ nvim/.config/nvim/lua/plugins/theme-eldritch.lua
@@ -25,11 +25,4 @@ return {
       colorscheme = "eldritch",
     },
   },
-  -- set ghostty syntax highlighting
-  {
-    dir = (vim.env.GHOSTTY_RESOURCES_DIR or "") .. "/../vim/vimfiles",
-    lazy = false, -- Ensures it loads for Ghostty config detection
-    name = "ghostty", -- Avoids the name being "vimfiles"
-    cond = vim.env.GHOSTTY_RESOURCES_DIR ~= nil, -- Only load if Ghostty is installed
-  },
 }
