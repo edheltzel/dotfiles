468b0c132da999483fffa17efc155184226bca91
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-07-12 14:24:48 -0400

    remove obsolete `ghostty-syntax.lua` and integrate its functionality into `lang-support.lua` for improved organization

--- nvim/.config/nvim/lua/plugins/lang-ghostty-syntax.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    dir = (vim.env.GHOSTTY_RESOURCES_DIR or "") .. "/../vim/vimfiles",
-    lazy = false, -- Ensures it loads for Ghostty config detection
-    name = "ghostty", -- Avoids the name being "vimfiles"
-    cond = vim.env.GHOSTTY_RESOURCES_DIR ~= nil, -- Only load if Ghostty is installed
-  },
-}
--- nvim/.config/nvim/lua/plugins/lang-support.lua
+++ nvim/.config/nvim/lua/plugins/lang-support.lua
@@ -10,4 +10,10 @@ return {
     opts = {},
     build = "make setup",
   },
+  {
+    dir = (vim.env.GHOSTTY_RESOURCES_DIR or "") .. "/../vim/vimfiles",
+    lazy = false, -- Ensures it loads for Ghostty config detection
+    name = "ghostty", -- Avoids the name being "vimfiles"
+    cond = vim.env.GHOSTTY_RESOURCES_DIR ~= nil, -- Only load if Ghostty is installed
+  },
 }
