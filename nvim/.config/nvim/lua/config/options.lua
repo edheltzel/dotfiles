e0aecc63efeece3690ce113aba6772f0eb128bb8
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-07-16 14:11:57 -0400

    neovim options updated for file handling
    turning off backup creations of files and swaps
    - addsin suppor

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,6 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 --
+-- file handling  -------------------------------------------------------------------------------
+vim.opt.backup = false -- dont create backup files
+vim.opt.writebackup = false -- dont create backup files
+vim.opt.swapfile = false -- dont create swap files
+
+-- Enable persistent undo, allows an undo history across sessions.
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.expand("~/.local/share/nvim/undodir") -- set undo directory
+
 -- visual settings  -------------------------------------------------------------------------------
 vim.opt.cursorcolumn = true
 vim.opt.colorcolumn = "100"
