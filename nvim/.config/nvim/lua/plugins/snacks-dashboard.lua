0ec6fcf71d463bfe184aee85d45379b4e91ce73b
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-07-16 15:23:06 -0400

    trying to be more minimal

--- nvim/.config/nvim/lua/plugins/snacks-dashboard.lua
+++ nvim/.config/nvim/lua/plugins/snacks-dashboard.lua
@@ -7,29 +7,21 @@ return {
           keys = {
             { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
             { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            -- { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            -- { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
             { icon = " ", key = "o", desc = "Find Session", action = ":SessionSearch" },
             { icon = "⊙ ", key = "d", desc = "DOTFILES", action = ":cd ~/.dotfiles | :e ." },
-            { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+            -- { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
             { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
             { icon = " ", key = "q", desc = "Quit", action = ":qa" },
           },
           header = [[
-    
-
-
      ███╗   ██╗███████╗ ██████╗    ███████╗██████╗ 
      ████╗  ██║██╔════╝██╔═══██╗   ██╔════╝██╔══██╗
      ██╔██╗ ██║█████╗  ██║   ██║   █████╗  ██║  ██║
      ██║╚██╗██║██╔══╝  ██║   ██║   ██╔══╝  ██║  ██║
      ██║ ╚████║███████╗╚██████╔╝██╗███████╗██████╔╝
-     ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═╝╚══════╝╚═════╝ 
-     
-     
-     
-     
-    ]],
+     ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═╝╚══════╝╚═════╝ ]],
         },
       },
     },
