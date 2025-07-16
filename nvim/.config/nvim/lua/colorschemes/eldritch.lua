77d0cb226df483aee8933bd39ba772c0490f9de2
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-07-16 19:11:02 -0400

    neovim looks better with eldritch not being transparent

--- nvim/.config/nvim/lua/colorschemes/eldritch.lua
+++ nvim/.config/nvim/lua/colorschemes/eldritch.lua
@@ -7,7 +7,7 @@ return {
     priority = 1000,
     opts = {
       palette = "default",
-      transparent = true,
+      transparent = false ,
       styles = {
         sidebars = "dark",
         floats = "dark",
