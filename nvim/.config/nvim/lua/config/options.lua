9be8a099ab8a732e3cd241a93e815b4619d754f0
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-07-16 13:27:44 -0400

    remove commented out options for neovim

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,13 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 --
--- file handling  -------------------------------------------------------------------------------
-vim.opt.backup = false -- dont create backup files
-vim.opt.writebackup = false -- dont create backup files
-vim.opt.swapfile = false -- dont create swap files
-vim.opt.undofile = true -- persistent undo
-vim.opt.undodir = vim.fn.expand("~/.local/share/nvim/undodir") -- set undo directory
-
 -- visual settings  -------------------------------------------------------------------------------
 vim.opt.cursorcolumn = true
 vim.opt.colorcolumn = "100"
