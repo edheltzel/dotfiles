84d06061655891bf4a34d813d98bdd2c45826ded
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-07-16 15:22:56 -0400

    resolved warning and error with auto sessions through the LazyHealth command

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -3,12 +3,12 @@ return {
     "rmagatti/auto-session",
     lazy = false,
 
-    ---enables autocomplete for opts
-    ---@module "auto-session"
-    ---@type AutoSession.Config
-    opts = {
-      suppressed_dirs = { "~/", "~/Downloads", "/" },
-      -- log_level = 'debug',
-    },
+    config = function()
+      vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+      require('auto-session').setup({
+        suppressed_dirs = { "~/", "/", "~/Downloads", "~/Developer/", "~/Sites/", "~/Documents/" },
+        -- log_level = 'debug',
+      })
+    end
   },
 }
