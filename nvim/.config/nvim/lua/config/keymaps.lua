ef87fb9b967fbc3f39040934f756f3dab24d2a36
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-07-18 17:55:07 -0400

    setting column to 120 and formatting

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -28,7 +28,6 @@ keymap.set({ "n", "v" }, "<leader>d", "d", { desc = "Delete without yanking" })
 -- ESCAPE INSERT MODE
 keymap.set("i", "jj", "<Esc>", { desc = "Exit INSERT mode with jj" })
 keymap.set("i", "jk", "<Esc>", { desc = "Exit INSERT mode with jk" })
-
 keymap.set("n", "U", "<C-r>", { desc = "Redo" }) -- redo with U
 keymap.set("n", "<D-z>", "u", { desc = "Redo" }) -- undo with cmd+z
 -- JUMP to BOL/EOL
@@ -36,7 +35,6 @@ keymap.set("n", "gh", "^", { desc = "Jump beginning of line" })
 keymap.set("n", "gl", "$", { desc = "Jump end of line" })
 -- CLEAR SEARCH HIGHLIGHTS
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-
 keymap.set("v", "<", "<gv", { desc = "Indent left and reslect" })
 keymap.set("v", ">", ">gv", { desc = "Indent right and reslect" })
 
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,15 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 --
+local option = vim.opt
 -- file handling  -------------------------------------------------------------------------------
-vim.opt.backup = false -- dont create backup files
-vim.opt.writebackup = false -- dont create backup files
-vim.opt.swapfile = false -- dont create swap files
-
--- Enable persistent undo, allows an undo history across sessions.
-vim.opt.undofile = true
-vim.opt.undodir = vim.fn.expand("~/.local/share/nvim/undodir") -- set undo directory
+option.backup = false -- dont create backup files
+option.writebackup = false -- dont create backup files
+option.swapfile = false -- dont create swap files
+option.undofile = true -- Enable persistent undo, allows an undo history across sessions.
+option.undodir = vim.fn.expand("~/.local/share/nvim/undodir") -- set undo directory
 
 -- visual settings  -------------------------------------------------------------------------------
-vim.opt.cursorcolumn = true
-vim.opt.colorcolumn = "100"
+option.cursorcolumn = true
+option.colorcolumn = "120"
