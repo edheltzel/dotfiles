e0b241b56c808a6142510b89bba5f0eac18d1eac
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-07-28 08:24:40 -0400

    fish var update to have manpages open in neovim

--- fish/.config/fish/conf.d/variables.fish
+++ fish/.config/fish/conf.d/variables.fish
@@ -9,17 +9,20 @@ set -gx XDG_MUSIC_DIR $HOME/Music
 set -gx XDG_PICTURES_DIR $HOME/Pictures
 set -gx XDG_VIDEOS_DIR $HOME/Videos
 
-# cargo - IDK if this is necessary
+# cargo
 set -gx CARGO_TARGET_DIR /var/folders/88/3h9cyc4979d2l6p7xkn79yqr0000gn/T/cargo-installx9AWBr
 
-# Default Editor use code, nvim, etc.
-set -gx EDITOR code
+# Opan MAN pages in Neovim
+set -gx MANPAGER "nvim +Man!"
 
 # Default Terminal use kitty, wezterm, ghostty, etc.
 set -gx TERMINAL kitty
 
 string match -q "$TERM_PROGRAM" nvim
 and . (nvim --locate-shell-integration-path fish)
+
+# Default Editor use code, nvim, etc.
+set -gx EDITOR code
 
 # Volumes
 set -gx VOL xxx
