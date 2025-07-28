e2868a905bbdb0edf2d46f07a0619368e99b65e0
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-07-28 14:03:28 -0400

    reverting back to original behavior

--- fish/.config/fish/functions/cd.fish
+++ fish/.config/fish/functions/cd.fish
@@ -2,8 +2,10 @@ function cd --description 'list files in the current directory after changing to
     if count $argv >/dev/null
         # prevents recurse infinitely by using built-in cd
         builtin cd "$argv"; and eza --long --all --header --git --icons --no-permissions --no-time --no-user --no-filesize --group-directories-first
+        # builtin cd "$argv"; and yazi
     else
         # builtin cd ~; and eza -lagh --icons --group-directories-first
         builtin cd ~; and eza --long --all --header --git --icons --no-permissions --no-time --no-user --no-filesize --group-directories-first
+        # builtin cd "$argv"; and yazi
     end
 end
