7b8a767afa102af118a3fa089131ddc1bc7212c2
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-07-29 13:29:00 -0400

    neovim update - remove smoka7/multicursors.nvim, sticking with vim-visual-multi ctrl+n works well right now

--- nvim/.config/nvim/lua/plugins/multi-cursors.lua
+++ nvim/.config/nvim/lua/plugins/multi-cursors.lua
@@ -1,19 +1,4 @@
 return {
-  -- "smoka7/multicursors.nvim",
-  -- event = "VeryLazy",
-  -- dependencies = {
-  --   "nvimtools/hydra.nvim",
-  -- },
-  -- opts = {},
-  -- cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
-  -- keys = {
-  --   {
-  --     mode = { "v", "n" },
-  --     "<Leader>m",
-  --     "<cmd>MCstart<cr>",
-  --     desc = "Create a selection for selected text or word under the cursor",
-  --   },
-  -- },
   {
     "mg979/vim-visual-multi",
     event = "VeryLazy",
