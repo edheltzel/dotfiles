20268695b1bf750aa09a4c3027ddf87a4e61556f
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-08-04 08:07:14 -0400

    raycast script update fieldnotes to open the FNHome note

---
+++ config/.config/raycast/_enabled-commands/open-fieldnotes.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title FieldNotes
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 🌾
+# @raycast.packageName Launch Apple Notes with FiledNotes Home Note
+# @raycast.needsConfirmation false
+
+# Documentation:
+# @raycast.description Open Notes on the FieldNotes Home Note
+# @raycast.author edheltzel
+# @raycast.authorURL https://raycast.com/edheltzel
+
+osascript <<EOD
+tell application "Notes"
+    activate
+    try
+        set noteToOpen to first note whose name is "✱✱ Field Notes"
+        show noteToOpen
+    on error
+        # Fallback if note isn't found
+        log "Note not found"
+    end try
+end tell
+EOD
