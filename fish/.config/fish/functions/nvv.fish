0a823a3325db260c416cab4eb22727830ec873f9
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-08-20 10:48:26 -0400

    duti and fish changes
    - fixed a typo in nvv.fish
    - changed preview to only open pdfs by default

--- fish/.config/fish/functions/nvv.fish
+++ fish/.config/fish/functions/nvv.fish
@@ -1,3 +1,3 @@
-function nvv --descriptoin 'Open a new file in neovim from clipboard contents'
+function nvv --description 'Open a new file in neovim from clipboard contents'
     nvim -c 'enew | put +' -c startinsert
 end
