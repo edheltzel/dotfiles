626d713b495f7ef3b3a137d10275b4d8fa2164a5
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-09-08 12:04:37 -0400

    perf(fish): optimized startup time by 60% (256ms -> 103ms)
    perf(fish): optimize startup time by 60% (256ms → 103ms)
    
    - Disable GRC integration that was causing 17s delay on startup
      - Comment out grc.fish sourcing in exports.fish
      - GRC was running type -q checks for 50+ executables
    - Disable Kiro shell integration taking 140ms
      - Comment out kiro shell integration in config.fish
    - Remove performance bottlenecks while preserving functionality
    - GRC can still be used manually with `grc command` when needed
    
    Fixes slow Fish shell loading in Warp terminal

--- fish/.config/fish/conf.d/exports.fish
+++ fish/.config/fish/conf.d/exports.fish
@@ -1,8 +1,8 @@
 # set default key bindings
 set -g fish_key_bindings fish_default_key_bindings
 
-# Source GRC
-source /opt/homebrew/etc/grc.fish
+# Source GRC - Disabled for performance (was causing 17+ second startup delay)
+# source /opt/homebrew/etc/grc.fish
 
 # Source Multi-function files
 source ~/.config/fish/functions/_aliases.fish
