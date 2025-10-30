47781a8a9dfe27e61b0150d6f3fdccb6ed3d3347
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-10-30 13:35:09 -0400

    adds ai plugins to neovim

---
+++ nvim/.config/nvim/lua/plugins/opencode.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "NickvanDyke/opencode.nvim",
+    dependencies = {
+      {
+        "folke/snacks.nvim",
+        opts = {
+          input = {},
+          picker = {},
+          terminal = {},
+        },
+      },
+    },
+    config = function()
+      --- @type opencode.Opts
+      vim.g.opencode_opts = {
+        -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
+      }
+
+      -- Required for `opts.auto_reload`.
+      vim.o.autoread = true
+
+      -- Recommended/example keymaps.
+      vim.keymap.set({ "n", "x" }, "<C-a>", function()
+        require("opencode").ask("@this: ", { submit = true })
+      end, { desc = "Ask opencode" })
+
+      vim.keymap.set({ "n", "x" }, "<C-x>", function()
+        require("opencode").select()
+      end, { desc = "Execute opencode action…" })
+
+      vim.keymap.set({ "n", "x" }, "ga", function()
+        require("opencode").prompt("@this")
+      end, { desc = "Add to opencode" })
+
+      vim.keymap.set("n", "<leader>ao", function()
+        require("opencode").toggle()
+      end, { desc = "Toggle opencode" })
+    end,
+  },
+}
