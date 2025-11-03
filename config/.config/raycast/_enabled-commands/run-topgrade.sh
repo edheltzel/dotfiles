f53d0a891ab9e556a5034e65836cf28cfb61120f
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-11-03 09:09:15 -0500

    run-topgrade now opens wezterm
    - using wezterm nightly

--- config/.config/raycast/_enabled-commands/run-topgrade.sh
+++ config/.config/raycast/_enabled-commands/run-topgrade.sh
@@ -23,18 +23,14 @@
 # osascript -e 'tell application "System Events" to key code 36' # Press Enter
 
 # Quick Access Mode: this will open a kitty Quick Access window and run topgrade command
-osascript -e 'tell application "System Events" to keystroke "t" using {control down, option down, command down}'
-sleep 1 # Wait for quick access top open
-osascript -e 'tell application "System Events" to keystroke "topgrade"' # Run topgrade
-osascript -e 'tell application "System Events" to key code 36' # Press Enter
-
-# Quick Access Mode: this will open a ghostty Quick Terminal window and run topgrade command
-# open -a ghostty
-# sleep 0.25 # Wait for quick access top open
-# # Close any window that might have opened
-# osascript -e 'tell application "Ghostty" to close every window'
-# sleep 0.25
 # osascript -e 'tell application "System Events" to keystroke "t" using {control down, option down, command down}'
-# sleep 0.25                                                              # Wait for quick access top open
+# sleep 1                                                                 # Wait for quick access top open
 # osascript -e 'tell application "System Events" to keystroke "topgrade"' # Run topgrade
 # osascript -e 'tell application "System Events" to key code 36'          # Press Enter
+
+# Application Mode: this will open a Wezterm window, create a new tab and run topgrade command
+osascript -e 'tell application "System Events" to keystroke "t" using {shift down,control down, option down, command down}'
+sleep 1                                                                               # Wait for quick access top open
+osascript -e 'tell application "System Events" to keystroke "t" using {command down}' # Create new tab
+osascript -e 'tell application "System Events" to keystroke "topgrade"'               # Run topgrade
+osascript -e 'tell application "System Events" to key code 36'                        # Press Enter
