c1be7c1fcafc123e381ff052e333bafd834ce7e1
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2025-11-13 09:18:41 -0500

    only using wezterm - run topgrade from raycast now uses wezterm

--- config/.config/raycast/_enabled-commands/run-topgrade.sh
+++ config/.config/raycast/_enabled-commands/run-topgrade.sh
@@ -15,22 +15,15 @@
 # @raycast.author edheltzel
 # @raycast.authorURL https://raycast.com/edheltzel
 
-# Application Mode: this will open a kitty window and run topgrade command
-# open -a kitty
-# sleep 1 # Wait for terminal to open
-# osascript -e 'tell application "kitty" to activate'
-# osascript -e 'tell application "System Events" to keystroke "topgrade"'
-# osascript -e 'tell application "System Events" to key code 36' # Press Enter
-
-# Quick Access Mode: this will open a kitty Quick Access window and run topgrade command
-osascript -e 'tell application "System Events" to keystroke "t" using {control down, option down, command down}'
-sleep 1                                                                 # Wait for quick access top open
-osascript -e 'tell application "System Events" to keystroke "topgrade"' # Run topgrade
-osascript -e 'tell application "System Events" to key code 36'          # Press Enter
+# Kitty Quick Access Mode: this will open a kitty Quick Access window and run topgrade command
+# osascript -e 'tell application "System Events" to keystroke "t" using {control down, option down, command down}'
+# sleep 1                                                                 # Wait for quick access top open
+# osascript -e 'tell application "System Events" to keystroke "topgrade"' # Run topgrade
+# osascript -e 'tell application "System Events" to key code 36'          # Press Enter
 
 # Application Mode: this will open a Wezterm window, create a new tab and run topgrade command
-# osascript -e 'tell application "System Events" to keystroke "t" using {shift down,control down, option down, command down}'
-# sleep 1                                                                               # Wait for quick access top open
-# osascript -e 'tell application "System Events" to keystroke "t" using {command down}' # Create new tab
-# osascript -e 'tell application "System Events" to keystroke "topgrade"'               # Run topgrade
-# osascript -e 'tell application "System Events" to key code 36'                        # Press Enter
+osascript -e 'tell application "System Events" to keystroke "t" using {shift down,control down, option down, command down}'
+sleep 1                                                                               # Wait for Wezterm open
+osascript -e 'tell application "System Events" to keystroke "t" using {command down}' # Create new tab
+osascript -e 'tell application "System Events" to keystroke "topgrade"'               # Run topgrade
+osascript -e 'tell application "System Events" to key code 36'                        # Press Enter
--- fish/.config/fish/functions/_aliases.fish
+++ fish/.config/fish/functions/_aliases.fish
@@ -62,7 +62,6 @@ alias du dua
 # alias lzip 'ouch list'
 alias wget 'wget -c'
 alias mux zellij
-alias wz wezterm
 
 # Network shortcuts/aliases and utilities
 alias ip "dig +short myip.opendns.com @resolver1.opendns.com" # dumps [YOUR PUBLIC IP] [URL IP]
