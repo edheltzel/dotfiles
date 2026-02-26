e769e22bf0409ae005f8f59ffcfe8a48e3479bc3
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2026-02-26 12:01:05 -0500

    fabric alias update to fix typo

--- fish/.config/fish/functions/_aliases.fish
+++ fish/.config/fish/functions/_aliases.fish
@@ -30,7 +30,7 @@ alias fld "cd \"/Users/ed/Library/Mobile Documents/iCloud~md~obsidian/Documents/
 
 # ADE ( AI/Agent  Development Environment )
 alias atlas="bun $HOME/.claude/skills/PAI/Tools/pai.ts"
-alias fabic="fabric-ai"
+alias fabric="fabric-ai"
 
 # Network shortcuts/aliases and utilities
 alias ip "dig +short myip.opendns.com @resolver1.opendns.com" # dumps [YOUR PUBLIC IP] [URL IP]
