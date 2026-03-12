4eba9afd776143215d9a341cd8c75c85b594acbe
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2026-03-12 16:38:39 -0400

    alt key for wezterm updated

--- config/.config/wezterm/configuration.lua
+++ config/.config/wezterm/configuration.lua
@@ -15,6 +15,8 @@ local function setup(theme, keymaps)
   -- Input
   config.enable_kitty_keyboard = true
   config.enable_csi_u_key_encoding = false
+  config.send_composed_key_when_left_alt_is_pressed = false
+  config.send_composed_key_when_right_alt_is_pressed = false
 
   -- Rendering
   config.front_end = "WebGpu"
