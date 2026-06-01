f4046f2c45fc4a925235ddf1ae2655085f6696c2
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2026-06-01 09:15:15 -0400

    adds vite abbr

--- fish/.config/fish/conf.d/abbr.fish
+++ fish/.config/fish/conf.d/abbr.fish
@@ -7,32 +7,40 @@ if status is-interactive
     # Directories
     # abbr --add cls '$DROPBOX/Clients'
     abbr --add config '~/.config/'
-    abbr --add local '~/.local/'
-    abbr --add wall '~/.wallpapers/'
+    abbr --add dev 'cd ~/Developer'
     abbr --add dls '~/Downloads/'
+    abbr --add dots 'cd ~/.dotfiles'
+    abbr --add fld "cd \"/Users/ed/Library/Mobile Documents/iCloud~md~obsidian/Documents/FieldNotes✱\""
     abbr --add goo 'cd ~/.go/'
+    abbr --add local '~/.local/'
+    abbr --add neo 'cd ~/.dotfiles/neoed/.config/nvim'
+    abbr --add neoed 'cd ~/.dotfiles/neoed/.config/nvim'
     abbr --add pg 'cd ~/Developer/_playground/'
     abbr --add sb 'cd ~/Developer/_sandboxes/'
-    abbr --add fld "cd \"/Users/ed/Library/Mobile Documents/iCloud~md~obsidian/Documents/FieldNotes✱\""
+    abbr --add sites 'cd ~/Sites'
+    abbr --add wall '~/.wallpapers/'
+    abbr --add work 'cd ~/Developer'
 
     # Editors/Terminals
+    abbr --add dc 'docker compose'
+    abbr --add dk docker
     abbr --add gty ghostty
+    abbr --add has --set-cursor 'herdr session attach %'
+    abbr --add hhs --set-cursor 'herdr --session %'
+    abbr --add hks --set-cursor 'herdr session stop %'
+    abbr --add hls 'herdr session list'
+    abbr --add hrd herdr
+    abbr --add hrr --set-cursor 'herdr --remote %'
+    abbr --add hss 'herdr server stop'
+    abbr --add hup 'herdr update'
     abbr --add mux tmux
     abbr --add muxa 'tmux attach'
     abbr --add muxaa 'tmux attach -t'
-    abbr --add muxs 'tmux new-session -s'
+    abbr --add muxd 'tmux detach'
     abbr --add muxk 'tmux kill-session -t'
     abbr --add muxka 'tmux kill-server'
     abbr --add muxl 'tmux list-sessions'
-    abbr --add muxd 'tmux detach'
-    abbr --add hrd herdr
-    abbr --add hup 'herdr update'
-    abbr --add has --set-cursor 'herdr session attach %'
-    abbr --add hrr --set-cursor 'herdr --remote %'
-    abbr --add hks --set-cursor 'herdr session stop %'
-    abbr --add hhs --set-cursor 'herdr --session %'
-    abbr --add hss 'herdr server stop'
-    abbr --add hls 'herdr session list'
+    abbr --add muxs 'tmux new-session -s'
 
     # AI Agents + Models
     abbr --add cc claude
@@ -51,54 +59,28 @@ if status is-interactive
     abbr --add v nvim
     abbr --add vv 'nvim .'
     abbr --add obs obsidian
+    abbr --add hostfile 'sudo nvim /etc/hosts'
+    abbr --add editssh 'nvim ~/.ssh'
 
     # Utilities
     abbr --add atm fastfetch
-    abbr --add cat 'bat -pp'
+    abbr --add cat 'bat -p'
     abbr --add link 'ln -s'
     abbr --add symlink 'ln -s'
-
-    # Git
-    abbr --add lg lazygit
-    abbr --add lj lazyjj
-    abbr --add ghw 'gh repo view --web'
-    abbr --add ghd 'gh dash'
-    abbr --add ghpr 'gh pr create -a "@me" --fill'
-    abbr --add ghm --set-cursor 'gh pr merge % --merge'
-    abbr --add ghr --set-cursor 'gh release create v% --generate-notes --latest'
-
-    # Package Managers 
-    abbr --add brews 'brew list'
-    abbr --add casks 'brew list --cask'
-    abbr --add cargos 'cargo install --list'
-    abbr --add gems 'gem list'
-    abbr --add npms 'npm list -g --depth=0'
-    abbr --add pns 'pnpm list -g'
-    abbr --add vps 'vp list -g'
-    abbr --add buns 'bun pm ls -g'
     abbr --add siz 'du -khsc'
     abbr --add sp 'speedtest -u Gbps'
     abbr --add spp 'speedtest -u Gbps'
-    abbr --add newcode 'bunx --package yo --package generator-code -- yo code'
     abbr --add yy yazi
     abbr --add wr wrangler
     abbr --add upp 'topgrade --yes'
     abbr --add bup 'topgrade --yes --only brew_formula brew_cask'
-    abbr --add phps 'php -S localhost:8888'
     abbr --add zip 'ouch compress -q'
     abbr --add unzip ouch decompress
     abbr --add lzip ouch ligst
-
-    # Migrated from aliases (simple 1:1 mappings)
+    abbr --add phps 'php -S localhost:8888'
     abbr --add md 'mkdir -p'
     abbr --add cwd pwd
     abbr --add yp 'pwd | pbcopy'
-    abbr --add dev 'cd ~/Developer'
-    abbr --add work 'cd ~/Developer'
-    abbr --add sites 'cd ~/Sites'
-    abbr --add dots 'cd ~/.dotfiles'
-    abbr --add neoed 'cd ~/.dotfiles/neoed/.config/nvim'
-    abbr --add neo 'cd ~/.dotfiles/neoed/.config/nvim'
     abbr --add e '$EDITOR'
     abbr --add o open
     abbr --add oo 'open .'
@@ -109,11 +91,36 @@ if status is-interactive
     abbr --add du dua
     abbr --add wget 'wget -c'
     abbr --add whois 'grc whois'
-    abbr --add hostfile 'sudo nvim /etc/hosts'
-    abbr --add editssh 'nvim ~/.ssh'
-    abbr --add dk docker
-    abbr --add dc 'docker compose'
-    abbr --add pn pnpm
-    abbr --add px 'pnpm dlx'
 
+    # Git
+    abbr --add lg lazygit
+    abbr --add lj lazyjj
+    abbr --add ghw 'gh repo view --web'
+    abbr --add ghd 'gh dash'
+    abbr --add ghpr 'gh pr create -a "@me" --fill'
+    abbr --add ghm --set-cursor 'gh pr merge % --merge'
+    abbr --add ghr --set-cursor 'gh release create v% --generate-notes --latest'
+
+    #JuJitsu (jj) - bookmarks = branch workspace = worktrees (sort-of)
+    abbr --add jjc --set-cursor 'jj commit -m "%"'
+    abbr --add jje --set-cursor 'jj edit %' #like git checkout
+    abbr --add jjn --set-cursor 'jj new %' #like git stash
+    abbr --add jjb --set-cursor 'jj bookmark create % -r @' #like git checkout -b
+    abbr --add jjs --set-cursor 'jj squash %'
+    abbr --add jju --set-cursor 'jj undo %'
+
+    # Package Managers 
+    abbr --add brews 'brew list'
+    abbr --add buns 'bun pm ls -g'
+    abbr --add cargos 'cargo install --list'
+    abbr --add casks 'brew list --cask'
+    abbr --add gems 'gem list'
+    abbr --add npms 'npm list -g --depth=0'
+    abbr --add pn pnpm
+    abbr --add pns 'pnpm list -g'
+    abbr --add px 'pnpm dlx'
+    abbr --add vps 'vp list -g'
+    abbr --add vpi 'vp install'
+    abbr --add vpg 'vp install -g '
+    abbr --add vpu 'vp update -g '
 end
