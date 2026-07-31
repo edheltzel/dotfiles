89572ba6a98d047e07ab2bc9162b2758e73ad62f
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2026-07-31 12:10:52 -0400

    installed grok cli

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -74,3 +74,7 @@ fish_add_path /Users/ed/.local/bin
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+# >>> grok installer >>>
+fish_add_path $HOME/.grok/bin
+# <<< grok installer <<<
