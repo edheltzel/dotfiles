ccc5c58d97ec125966d02a3793448a08e170bbd9
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2026-08-10 07:03:06 -0400

    removed archieved and not used

--- config/archived/sketchybar/helpers/init.lua
+++
@@ -1,4 +0,0 @@
--- Add the sketchybar module to the package cpath
-package.cpath = package.cpath .. ";/Users/" .. os.getenv("USER") .. "/.local/share/sketchybar_lua/?.so"
-
-os.execute("(cd helpers && make)")
